30ff3c61ed0474a03279dfaf192c580fe97d8548
Marco Rieger <m.rieger@mittwald.de>
2023-12-06 11:48:14 +0100

    transform also

--- packages/components/dev/viteI18nPlugin.ts
+++ packages/components/dev/viteI18nPlugin.ts
@@ -2,6 +2,20 @@ import { PluginOption } from "vite";
 import path from "path";
 import * as fs from "fs";
 
+const generateComponentIntlContent = (directory: string): string => {
+  const langFileDir = path.join(directory, "..");
+  const langObject: string[] = [];
+  fs.readdirSync(langFileDir).forEach((file) => {
+    const fileContent = fs.readFileSync(path.join(langFileDir, file), {
+      encoding: "utf8",
+    });
+    const cleanKey = file.split("/").pop()?.split(".")[0];
+    langObject.push(`"${cleanKey}":${fileContent}`);
+  });
+
+  return `{${langObject.join(",")}}`;
+};
+
 export default {
   name: "handle-i18n-import",
   enforce: "pre",
@@ -15,28 +29,27 @@ export default {
       });
     }
   },
+  async transform(source, id) {
+    if (!id.endsWith("intl/*.json")) {
+      return;
+    }
+
+    return {
+      code: generateComponentIntlContent(id),
+      map: null,
+    };
+  },
   async load(id) {
     if (!id.endsWith("intl/*.json")) {
       return;
     }
 
-    const langFileDir = path.join(id, "..");
-    const langObject: string[] = [];
-    fs.readdirSync(langFileDir).forEach((file) => {
-      const fileContent = fs.readFileSync(path.join(langFileDir, file), {
-        encoding: "utf8",
-      });
-      const cleanKey = file.split("/").pop()?.split(".")[0];
-      langObject.push(`"${cleanKey}":${fileContent}`);
-    });
-
-    return `{${langObject.join(",")}}`;
+    return generateComponentIntlContent(id);
   },
   resolveId: (id, source) => {
     if (id === "./intl/*.json" && source) {
       return {
         id: `${path.join(source, "..", id)}`,
-        external: "relative",
       };
     }
   },
