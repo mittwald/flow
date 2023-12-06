9a713374a017ac07ac239529483dee19f0b147d9
Marco Rieger <m.rieger@mittwald.de>
2023-12-06 10:30:58 +0100

    add i18n example

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 import styles from "./styles.module.css";
 import { testLib } from "@/lib/testLib.js";
+import intlMessages from "./intl/*.json";
+import { useLocalizedStringFormatter } from "react-aria";
 
 interface Props {
   foo: boolean;
@@ -8,10 +10,13 @@ interface Props {
 
 export const Button: FC<Props> = (props) => {
   testLib();
+
+  const stringFormatter = useLocalizedStringFormatter(intlMessages);
+
   return (
     <div className={styles.root}>
-      Button {props.foo ? "with" : "without"}{" "}
-      <span className={styles.foo}>foo</span>
+      Button {stringFormatter.format("example")}{" "}
+      {props.foo ? "with" : "without"} <span className={styles.foo}>foo</span>
     </div>
   );
 };
