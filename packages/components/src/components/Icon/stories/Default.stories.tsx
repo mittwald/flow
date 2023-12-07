fae678681130e1877daacbb3dc3ff6e3f95345e6
lmeyer <l.meyer@mittwald.de>
2023-12-07 11:24:07 +0100

    merge master

--- packages/components/src/components/Icon/stories/Default.stories.tsx
+++ packages/components/src/components/Icon/stories/Default.stories.tsx
@@ -3,6 +3,7 @@ import Icon from "@/components/Icon/Icon";
 import { faStar } from "@fortawesome/free-regular-svg-icons/faStar";
 
 const meta: Meta<typeof Icon> = {
+  title: "Icon",
   component: Icon,
 };
 export default meta;
