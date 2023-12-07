9532827560dcb2e8fa0cf5a039e8535745233b6a
lmeyer <l.meyer@mittwald.de>
2023-12-07 11:08:00 +0100

    resolve merge conflicts

--- packages/components/src/components/Text/Text.stories.tsx
+++ packages/components/src/components/Text/Text.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import Text from "./Text";
 
 const meta: Meta<typeof Text> = {
+  title: "Text",
   component: Text,
 };
 export default meta;
