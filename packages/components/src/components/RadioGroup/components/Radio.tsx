c3014306414347f2968f625ba0ff1fda5942bee1
lmeyer <l.meyer@mittwald.de>
2023-12-08 09:21:16 +0100

    add icon to radio group component

--- packages/components/src/components/RadioGroup/components/Radio.tsx
+++ packages/components/src/components/RadioGroup/components/Radio.tsx
@@ -2,22 +2,25 @@ import React, { FC, PropsWithChildren, ReactNode } from "react";
 import styles from "./Radio.module.css";
 import * as Aria from "react-aria-components";
 import clsx from "clsx";
-import Text from "@/components/Text/Text";
+import { Icon } from "@/components";
+import { IconLookup } from "@fortawesome/fontawesome-svg-core";
 
 export interface RadioProps
   extends PropsWithChildren<Omit<Aria.RadioProps, "children">> {
   description?: ReactNode;
+  icon?: IconLookup | ReactNode;
 }
 
 export const Radio: FC<RadioProps> = (props) => {
-  const { children, description, className, ...rest } = props;
+  const { children, description, icon, className, ...rest } = props;
 
   const rootClassName = clsx(className, styles.radio);
 
   return (
     <Aria.Radio {...rest} className={rootClassName}>
-      <Aria.Label>{children}</Aria.Label>
-      {description && <Text slot="description">{description}</Text>}
+      <span className={styles.title}>{children}</span>
+      {description && <span className={styles.description}>{description}</span>}
+      {icon && <Icon faIcon={icon} />}
     </Aria.Radio>
   );
 };
