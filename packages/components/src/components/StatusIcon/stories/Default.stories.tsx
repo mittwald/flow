99db7e7e1704841f5b0c5403682515cd11e6c47a
lmeyer <l.meyer@mittwald.de>
2024-01-10 08:31:26 +0100

    remove imports

--- packages/components/src/components/StatusIcon/stories/Default.stories.tsx
+++ packages/components/src/components/StatusIcon/stories/Default.stories.tsx
@@ -1,10 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import StatusIcon from "../StatusIcon";
-import React from "react";
-import { Heading } from "@/components/Heading";
-import { Content } from "@/components/Content";
-import { Icon } from "@/components/Icon";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const meta: Meta<typeof StatusIcon> = {
   title: "StatusIcon",
