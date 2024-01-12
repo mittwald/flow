74bd4c993ccd94eceab5291627085047ce2f1bb2
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-01-12 14:06:19 +0100

    Add checkbox component (#80)

---
+++ packages/components/src/components/Checkbox/stories/Default.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "../index";
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+const meta: Meta<typeof Checkbox> = {
+  title: "Checkbox",
+  component: Checkbox,
+  args: {
+    onChange: action("onChange"),
+  },
+  render: (props) => <Checkbox {...props}>Activate spam protection</Checkbox>,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = { args: { isDisabled: true } };
+
+export const DisabledSelected: Story = {
+  args: { isDisabled: true, isSelected: true },
+};
