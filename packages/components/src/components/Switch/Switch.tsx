70f460261b0ca7c2dfd65418f815d2bf8661fa45
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-01-12 15:04:28 +0100

    Add Switch Component (#90)

---
+++ packages/components/src/components/Switch/Switch.tsx
@@ -0,0 +1,38 @@
+import React, { FC, PropsWithChildren } from "react";
+import * as Aria from "react-aria-components";
+import styles from "./Switch.module.css";
+import clsx from "clsx";
+
+export interface SwitchProps
+  extends PropsWithChildren<Omit<Aria.SwitchProps, "children">> {
+  /** @default "trailing" */
+  labelPosition: "leading" | "trailing";
+  /** @default "accent" */
+  variant: "accent" | "negative";
+}
+
+export const Switch: FC<SwitchProps> = (props) => {
+  const {
+    children,
+    className,
+    labelPosition = "trailing",
+    variant = "accent",
+    ...rest
+  } = props;
+
+  const rootClassName = clsx(
+    className,
+    styles.root,
+    styles[`label-${labelPosition}`],
+    styles[variant],
+  );
+
+  return (
+    <Aria.Switch {...rest} className={rootClassName}>
+      <div className={styles.indicator} />
+      {children}
+    </Aria.Switch>
+  );
+};
+
+export default Switch;
