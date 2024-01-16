c47ff5f99fb03f4cc6efcfa20e071f1aeff68410
lmeyer <l.meyer@mittwald.de>
2024-01-16 11:34:02 +0100

    update get initials and fix bg color

--- packages/components/src/components/Initials/lib/getInitialsFromString.ts
+++ packages/components/src/components/Initials/lib/getInitialsFromString.ts
@@ -1,13 +1,9 @@
 export const getInitialsFromString = (initials: string): string[] => {
   return initials
+    .replace(/[^\p{L}\s]/giu, "")
     .split(" ")
-    .map((part) =>
-      part
-        ?.trim()
-        .split("")
-        .find((char) => /\p{L}/gu.test(char)),
-    )
-    .filter((char) => char !== undefined)
-    .map((char) => char!.toUpperCase())
+    .map((part) => part.trim()[0])
+    .filter((p) => p !== undefined)
+    .map((char) => char.toUpperCase())
     .slice(0, 2);
 };
