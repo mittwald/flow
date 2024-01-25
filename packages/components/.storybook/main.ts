1071145f3f2776cc19c7bd7dd6187f36357e9417
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-01-25 08:46:12 +0100

    chore(storybook): remove warnings in dev mode

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,23 +1,17 @@
-import { dirname, join } from "path";
-import { StorybookConfig } from "@storybook/react-vite";
-
-function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, "package.json")));
-}
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("storybook-addon-rtl-direction"),
-    getAbsolutePath("@storybook/addon-actions"),
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "storybook-addon-dir",
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  framework: "@storybook/react-vite",
+  core: {
+    builder: "@storybook/builder-vite",
   },
   docs: {
     autodocs: "tag",
