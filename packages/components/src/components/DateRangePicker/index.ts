86be7ef24a372650b1fef0a12c5f67235c7a7e9d
lmeyer <l.meyer@mittwald.de>
2024-01-25 11:30:17 +0100

    fix pipeline

--- packages/components/src/components/DateRangePicker/index.ts
+++ packages/components/src/components/DateRangePicker/index.ts
@@ -1,5 +1,3 @@
 import { DateRangePicker } from "./DateRangePicker";
 export { type DateRangePickerProps, DateRangePicker } from "./DateRangePicker";
-export * from "./components/DateRangeInput";
-export * from "./components/RangeCalendar";
 export default DateRangePicker;
