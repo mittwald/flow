a828747c0763f706492fa886763107c7f8ff6387
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-01-30 08:59:51 +0100

    ci: check if working is clean after build

--- packages/docs/src/lib/LiveCodeEditor/dynamicImports.ts
+++ packages/docs/src/lib/LiveCodeEditor/dynamicImports.ts
@@ -4,6 +4,6 @@ import { ImportMapping } from "@/lib/LiveCodeEditor/types";
 import { lazy } from "react";
 
 export const liveCodeEditorGlobalImports: ImportMapping = {
-  "Button:@mittwald/flow-next-components/Button": lazy(() => import("@mittwald/flow-next-components/Button")),
-"Link:@mittwald/flow-next-components/Link": lazy(() => import("@mittwald/flow-next-components/Link")),
+  "Link:@mittwald/flow-next-components/Link": lazy(() => import("@mittwald/flow-next-components/Link")),
+"Button:@mittwald/flow-next-components/Button": lazy(() => import("@mittwald/flow-next-components/Button")),
 };
