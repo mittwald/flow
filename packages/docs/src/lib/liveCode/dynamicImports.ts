9ee0a53d952a24e459b45a9e3efbbd5934b8b90c
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-01-31 17:01:46 +0100

    fix(docs): commit generated imports file

--- packages/docs/src/lib/liveCode/dynamicImports.ts
+++ packages/docs/src/lib/liveCode/dynamicImports.ts
@@ -4,6 +4,6 @@ import { ImportMapping } from "@/lib/liveCode/types";
 import { lazy } from "react";
 
 export const liveCodeEditorGlobalImports: ImportMapping = {
-  "Link:@mittwald/flow-next-components/Link": lazy(() => import("@mittwald/flow-next-components/Link")),
-"Button:@mittwald/flow-next-components/Button": lazy(() => import("@mittwald/flow-next-components/Button")),
+  "Button:@mittwald/flow-next-components/Button": lazy(() => import("@mittwald/flow-next-components/Button")),
+"Link:@mittwald/flow-next-components/Link": lazy(() => import("@mittwald/flow-next-components/Link")),
 };
