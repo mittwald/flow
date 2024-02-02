64e40197bc90a97f2675a29340012675a5c3bb06
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-02-02 09:16:58 +0100

    docs: use other demo SVG

--- packages/docs/src/content/02-components/button/examples/icon.tsx
+++ packages/docs/src/content/02-components/button/examples/icon.tsx
@@ -1,7 +1,7 @@
 import Button from "@mittwald/flow-next-components/Button";
 import Icon from "@mittwald/flow-next-components/Icon";
 
-<Button aria-label="Open menu">
+<Button aria-label="Star item">
   <Icon>
     <DemoSvg />
   </Icon>
