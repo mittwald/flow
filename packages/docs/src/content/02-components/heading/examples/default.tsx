643005bcce26d98f07e1bc1bdf035af1093bcf64
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-02-02 11:29:57 +0100

    docs(Heading): add docs

---
+++ packages/docs/src/content/02-components/heading/examples/default.tsx
@@ -0,0 +1,3 @@
+import Heading from "@mittwald/flow-next-components/Heading";
+
+<Heading>This is a heading</Heading>;
---
+++ packages/docs/src/content/02-components/heading/examples/levels.tsx
@@ -0,0 +1,9 @@
+import Heading from "@mittwald/flow-next-components/Heading";
+
+<>
+  <Heading level={1}>This is a level 1 heading</Heading>
+  <Heading level={2}>This is a level 2 heading</Heading>
+  <Heading level={3}>This is a level 3 heading</Heading>
+  <Heading level={4}>This is a level 4 heading</Heading>
+  <Heading level={5}>This is a level 5 heading</Heading>
+</>;
