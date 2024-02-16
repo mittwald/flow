cecf0ce28cd0606acc6053fc54646a5f4bf18616
lmeyer <l.meyer@mittwald.de>
2024-02-16 08:08:24 +0100

    update button example content

--- packages/docs/src/content/02-components/button/examples/variants.tsx
+++ packages/docs/src/content/02-components/button/examples/variants.tsx
@@ -1,9 +1,9 @@
 import Button from "@mittwald/flow-react-components/Button";
 
 <Row>
-  <Button variant="primary">Primary</Button>
-  <Button variant="secondary">Secondary</Button>
-  <Button variant="success">Success</Button>
-  <Button variant="danger">Danger</Button>
+  <Button variant="primary">Continue/Action</Button>
+  <Button variant="secondary">Abort/Back</Button>
+  <Button variant="success">Create/Save</Button>
+  <Button variant="danger">Delete/Terminate</Button>
   <Button variant="plain">Plain</Button>
 </Row>;
