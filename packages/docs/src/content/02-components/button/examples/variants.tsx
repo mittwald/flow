058f70d12f69fcdff3855a9a93993592797a6318
lmeyer <l.meyer@mittwald.de>
2024-02-16 07:28:45 +0100

    add border widths

--- packages/docs/src/content/02-components/button/examples/variants.tsx
+++ packages/docs/src/content/02-components/button/examples/variants.tsx
@@ -3,7 +3,7 @@ import Button from "@mittwald/flow-react-components/Button";
 <Row>
   <Button variant="primary">Primary</Button>
   <Button variant="secondary">Secondary</Button>
-  <Button variant="accent">Accent</Button>
+  <Button variant="success">Success</Button>
   <Button variant="danger">Danger</Button>
   <Button variant="plain">Plain</Button>
 </Row>;
