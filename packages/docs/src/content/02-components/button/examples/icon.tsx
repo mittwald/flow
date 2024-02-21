d9b4aeff94cef15597b62fe8b4b3f7eb99062ac5
lmeyer <l.meyer@mittwald.de>
2024-02-21 07:32:28 +0100

    fix: merge master

--- packages/docs/src/content/02-components/button/examples/icon.tsx
+++ packages/docs/src/content/02-components/button/examples/icon.tsx
@@ -1,6 +1,6 @@
 import Button from "@mittwald/flow-react-components/Button";
 import Icon from "@mittwald/flow-react-components/Icon";
-import { faStar } from "@fortawesome/free-regular-svg-icons/faStar";
+import faStar from "@fortawesome/free-regular-svg-icons/faStar";
 
 <Button aria-label="Star item">
   <Icon faIcon={faStar} />
