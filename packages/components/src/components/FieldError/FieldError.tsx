83ba4c961e9b97064e9db39f5fac76b6af0201e4
lmeyer <l.meyer@mittwald.de>
2024-02-28 11:22:13 +0100

    feat: update form control design tokens

--- packages/components/src/components/FieldError/FieldError.tsx
+++ packages/components/src/components/FieldError/FieldError.tsx
@@ -3,6 +3,7 @@ import styles from "./FieldError.module.scss";
 import * as Aria from "react-aria-components";
 import clsx from "clsx";
 import { useProps } from "@/lib/propsContext";
+import { IconDanger } from "@/components/Icon/components/icons";
 
 export interface FieldErrorProps
   extends PropsWithChildren<Omit<Aria.FieldErrorProps, "children">> {}
@@ -14,6 +15,7 @@ export const FieldError: FC<FieldErrorProps> = (props) => {
 
   return (
     <Aria.FieldError {...rest} className={rootClassName}>
+      <IconDanger size="s" />
       {children}
     </Aria.FieldError>
   );
