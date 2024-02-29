7e2af67dc4acf754032a061a49dbe55c04d12602
lmeyer <l.meyer@mittwald.de>
2024-02-29 06:38:43 +0100

    fix: remove unnecessary aria label

--- packages/docs/src/content/02-components/button/examples/iconText.tsx
+++ packages/docs/src/content/02-components/button/examples/iconText.tsx
@@ -2,7 +2,7 @@ import Button from "@mittwald/flow-react-components/Button";
 import { Text } from "@mittwald/flow-react-components/Text";
 import { IconChevronDown } from "@mittwald/flow-react-components/Icons";
 
-<Button aria-label="Star item">
+<Button>
   <Text>Add email address</Text>
   <IconChevronDown />
 </Button>;
