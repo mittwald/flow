b237d0f9a3f1ccb101366f0f5b43c3108c179439
lmeyer <l.meyer@mittwald.de>
2024-03-04 13:00:06 +0100

    feat: add inline link

---
+++ packages/docs/src/content/02-components/link/examples/inline.tsx
@@ -0,0 +1,3 @@
+import Link from "@mittwald/flow-react-components/Link";
+
+<Link inline>Inline link</Link>;
---
+++ packages/docs/src/content/02-components/link/examples/inlineText.tsx
@@ -0,0 +1,6 @@
+import Link from "@mittwald/flow-react-components/Link";
+import { Text } from "@mittwald/flow-react-components/Text";
+
+<Text>
+  Check our <Link>onboarding</Link> for more information.
+</Text>;
