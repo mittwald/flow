f6e4f515d34cdff94cf562ead44e0b9773288212
lmeyer <l.meyer@mittwald.de>
2024-03-07 11:48:45 +0100

    build: dynamic imports

--- packages/components/src/components/Button/stories/Default.stories.tsx
+++ packages/components/src/components/Button/stories/Default.stories.tsx
@@ -3,8 +3,6 @@ import Button from "../Button";
 import React from "react";
 import { IconPlus } from "@/components/Icon/components/icons";
 import { action } from "@storybook/addon-actions";
-import { Text } from "@/components/Text";
-import IconChevronDown from "@/components/Icon/components/icons/IconChevronDown";
 
 const meta: Meta<typeof Button> = {
   title: "Actions/Button",
@@ -53,24 +51,6 @@ export const SmallWithIcon: Story = {
   render: (props) => (
     <Button {...props} aria-label="Add to favorites" size="s">
       <IconPlus />
-    </Button>
-  ),
-};
-
-export const WithTextAndIcon: Story = {
-  render: (props) => (
-    <Button {...props}>
-      <Text>Add email address</Text>
-      <IconChevronDown />
-    </Button>
-  ),
-};
-
-export const SmallWithTextAndIcon: Story = {
-  render: (props) => (
-    <Button {...props} size="s">
-      <Text>Add email address</Text>
-      <IconChevronDown />
     </Button>
   ),
 };
--- packages/docs/src/lib/liveCode/dynamicImports.ts
+++ packages/docs/src/lib/liveCode/dynamicImports.ts
@@ -2,7 +2,7 @@
 /* auto-generated file */
 import { ImportMapping } from "@/lib/liveCode/types";
 import { lazy } from "react";
-import { NavigationItem as I23 } from "@mittwald/flow-react-components/Navigation";
+import { NavigationItem as I22 } from "@mittwald/flow-react-components/Navigation";
 
 export const liveCodeEditorGlobalImports: ImportMapping = {
   "ExampleSvg:@/content/01-getting-started/stylesheet/examples/components/ExampleSvg": lazy(() => import("@/content/01-getting-started/stylesheet/examples/components/ExampleSvg").then(module => ({ default: module.ExampleSvg } ))),
@@ -14,7 +14,6 @@ export const liveCodeEditorGlobalImports: ImportMapping = {
 "IconEmail:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconEmail } ))),
 "Button:@mittwald/flow-react-components/Button": lazy(() => import("@mittwald/flow-react-components/Button").then(module => ({ default: module.Button } ))),
 "IconHome:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconHome } ))),
-"IconChevronDown:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconChevronDown } ))),
 "Checkbox:@mittwald/flow-react-components/Checkbox": lazy(() => import("@mittwald/flow-react-components/Checkbox").then(module => ({ default: module.Checkbox } ))),
 "Heading:@mittwald/flow-react-components/Heading": lazy(() => import("@mittwald/flow-react-components/Heading").then(module => ({ default: module.Heading } ))),
 "Icon:@mittwald/flow-react-components/Icon": lazy(() => import("@mittwald/flow-react-components/Icon").then(module => ({ default: module.Icon } ))),
@@ -28,7 +27,7 @@ export const liveCodeEditorGlobalImports: ImportMapping = {
 "Link:@mittwald/flow-react-components/Link": lazy(() => import("@mittwald/flow-react-components/Link").then(module => ({ default: module.Link } ))),
 "LayoutCard:@mittwald/flow-react-components/LayoutCard": lazy(() => import("@mittwald/flow-react-components/LayoutCard").then(module => ({ default: module.LayoutCard } ))),
 "Navigation:@mittwald/flow-react-components/Navigation": lazy(() => import("@mittwald/flow-react-components/Navigation").then(module => ({ default: module.Navigation } ))),
-"NavigationItem:@mittwald/flow-react-components/Navigation": I23,
+"NavigationItem:@mittwald/flow-react-components/Navigation": I22,
 "IconCustomer:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconCustomer } ))),
 "IconProject:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconProject } ))),
 "IconServer:@mittwald/flow-react-components/Icons": lazy(() => import("@mittwald/flow-react-components/Icons").then(module => ({ default: module.IconServer } ))),
