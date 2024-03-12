ac9a2c1ac15bb80bfcddc614dc94198a8e4c7b5e
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-03-12 13:54:35 +0100

    feat: add ColumnLayout

--- packages/components/vite.build.config.ts
+++ packages/components/vite.build.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(
           Avatar: "./src/components/Avatar/index.ts",
           Button: "./src/components/Button/index.ts",
           Checkbox: "./src/components/Checkbox/index.ts",
+          ColumnLayout: "./src/components/ColumnLayout/index.ts",
           Content: "./src/components/Content/index.ts",
           CopyToClipboardButton:
             "./src/components/CopyToClipboardButton/index.ts",
