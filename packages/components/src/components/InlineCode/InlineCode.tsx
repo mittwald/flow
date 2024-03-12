e8c0a914a0556ce3e11c67d3b5f3159fde7e257f
lmeyer <l.meyer@mittwald.de>
2024-03-12 14:10:41 +0100

    refactor: replace span with code

--- packages/components/src/components/InlineCode/InlineCode.tsx
+++ packages/components/src/components/InlineCode/InlineCode.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import styles from "./InlineCode.module.scss";
 
 export interface InlineCodeProps
-  extends PropsWithChildren<ComponentProps<"span">> {}
+  extends PropsWithChildren<ComponentProps<"code">> {}
 
 export const InlineCode: FC<InlineCodeProps> = (props) => {
   const { children, className, ...rest } = props;
