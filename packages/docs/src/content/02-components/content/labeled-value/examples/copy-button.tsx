6506b76c43242a76a3351132a562689c8df3800f
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-03-15 10:09:29 +0100

    doc: add component sections to docs
    * docs: add component subcategories
    
    * refactor!: rename copy to clipboard button
    
    * docs: update inline code story
    
    * docs: add inline alert to custom components
    
    ---------
    
    Co-authored-by: Marco Falkenberg <m.falkenberg@mittwald.de>

--- packages/docs/src/content/02-components/content/labeled-value/examples/copy-button.tsx
+++ packages/docs/src/content/02-components/content/labeled-value/examples/copy-button.tsx
@@ -1,10 +1,10 @@
 import LabeledValue from "@mittwald/flow-react-components/LabeledValue";
 import Label from "@mittwald/flow-react-components/Label";
 import Content from "@mittwald/flow-react-components/Content";
-import CopyToClipboardButton from "@mittwald/flow-react-components/CopyToClipboardButton";
+import CopyButton from "@mittwald/flow-react-components/CopyButton";
 
 <LabeledValue>
   <Label>Project ID</Label>
   <Content>p-eWRhpRV</Content>
-  <CopyToClipboardButton text="Project ID: p-eWRhpRV" />
+  <CopyButton text="Project ID: p-eWRhpRV" />
 </LabeledValue>;
