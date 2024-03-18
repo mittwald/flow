643ba756cafde27e777bf18b9043eb7e263afa2b
lmeyer <l.meyer@mittwald.de>
2024-03-18 08:26:00 +0100

    docs: add stories

---
+++ packages/components/src/components/List/stories/ListItem.stories.tsx
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import List from "../List";
+import React from "react";
+import { Heading } from "@/components/Heading";
+import { Text } from "@/components/Text";
+import { Content } from "@/components/Content";
+import { ItemContextMenu } from "@/components/List/components/Items/components/ItemContextMenu";
+import { ContextMenuItem } from "@/components/ContextMenu";
+import defaultMeta from "./Default.stories";
+import { Item } from "@/components/List/components/Items/components/Item";
+import { Avatar } from "@/components/Avatar";
+import { IconEmail } from "@/components/Icon/components/icons";
+
+const meta: Meta<typeof List> = {
+  ...defaultMeta,
+  title: "Structure/List/ListItem",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof List>;
+
+export const WithoutAvatar: Story = {
+  render: () => (
+    <Item>
+      <Heading>John Doe</Heading>
+      <Text>Mittwald</Text>
+      <Content style={{ background: "lightgrey" }}></Content>
+      <ItemContextMenu>
+        <ContextMenuItem>Show details</ContextMenuItem>
+      </ItemContextMenu>
+    </Item>
+  ),
+};
+
+export const WithoutSubtitle: Story = {
+  render: () => (
+    <Item>
+      <Heading>John Doe</Heading>
+      <Content style={{ background: "lightgrey" }}></Content>
+      <ItemContextMenu>
+        <ContextMenuItem>Show details</ContextMenuItem>
+      </ItemContextMenu>
+    </Item>
+  ),
+};
+
+export const WithoutContent: Story = {
+  render: () => (
+    <Item>
+      <Avatar>
+        <IconEmail />
+      </Avatar>
+      <Heading>john@doe.de</Heading>
+      <ItemContextMenu>
+        <ContextMenuItem>Show details</ContextMenuItem>
+      </ItemContextMenu>
+    </Item>
+  ),
+};
+
+export const WithoutContextMenu: Story = {
+  render: () => (
+    <Item>
+      <Avatar>
+        <IconEmail />
+      </Avatar>
+      <Heading>john@doe.de</Heading>
+      <Content style={{ background: "lightgrey" }}></Content>
+    </Item>
+  ),
+};
