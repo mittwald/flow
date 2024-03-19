2becdb5a891a7ef263aad72027d65c8e24ae67d9
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-03-19 08:20:11 +0100

    feat: support Switch in Sections

--- packages/components/src/components/propTypes/index.ts
+++ packages/components/src/components/propTypes/index.ts
@@ -4,10 +4,7 @@ import type { IconProps } from "@/components/Icon";
 import type { LabelProps } from "@/components/Label";
 import type { ContentProps } from "@/components/Content";
 import type { LayoutCardProps } from "@/components/LayoutCard";
-import type {
-  NavigationItemProps,
-  NavigationProps,
-} from "@/components/Navigation";
+import type { NavigationItemProps } from "@/components/Navigation";
 import type { LinkProps } from "@/components/Link";
 import type { FieldErrorProps } from "@/components/FieldError";
 import type { FieldDescriptionProps } from "@/components/FieldDescription";
@@ -16,26 +13,27 @@ import type { HeadingProps } from "@/components/Heading";
 import type { InitialsProps } from "@/components/Initials";
 import type { ImageProps } from "@/components/Image";
 import type { CopyButtonProps } from "@/components/CopyButton";
+import { HeaderProps } from "@/components/Header/";
 
 export * from "./types";
 
 export interface FlowComponentPropsTypes {
-  Text: TextProps;
   Button: ButtonProps;
-  CopyButton: CopyButtonProps;
-  Icon: IconProps;
-  Label: LabelProps;
   Content: ContentProps;
-  LayoutCard: LayoutCardProps;
-  Navigation: NavigationProps;
-  NavigationItem: NavigationItemProps;
-  Heading: HeadingProps;
-  InlineAlert: InlineAlertProps;
-  Link: LinkProps;
-  Initials: InitialsProps;
-  Image: ImageProps;
-  FieldError: FieldErrorProps;
+  CopyButton: CopyButtonProps;
   FieldDescription: FieldDescriptionProps;
+  FieldError: FieldErrorProps;
+  Header: HeaderProps;
+  Heading: HeadingProps;
+  Icon: IconProps;
+  Image: ImageProps;
+  Initials: InitialsProps;
+  InlineAlert: InlineAlertProps;
+  Label: LabelProps;
+  LayoutCard: LayoutCardProps;
+  Link: LinkProps;
+  NavigationItem: NavigationItemProps;
+  Text: TextProps;
 }
 
 const propsContextSupportingComponentsMap: Record<
