0d11433d2d46b130115124cd3de9d6bf1472e42b
lmeyer <l.meyer@mittwald.de>
2024-03-19 13:15:56 +0100

    fix: add icon class in avatar

--- packages/components/src/components/Avatar/Avatar.tsx
+++ packages/components/src/components/Avatar/Avatar.tsx
@@ -24,6 +24,9 @@ export const Avatar: FC<AvatarProps> = (props) => {
     Initials: {
       className: styles.initials,
     },
+    Icon: {
+      className: styles.icon,
+    },
   };
 
   return (
