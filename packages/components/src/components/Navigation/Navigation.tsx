eb92a057d284c9151b49186f38e4f925547ad821
lmeyer <l.meyer@mittwald.de>
2024-03-21 13:52:58 +0100

    feat: add navigation group

--- packages/components/src/components/Navigation/Navigation.tsx
+++ packages/components/src/components/Navigation/Navigation.tsx
@@ -1,7 +1,14 @@
 import React, { ComponentProps, FC, PropsWithChildren } from "react";
 import styles from "./Navigation.module.scss";
 import clsx from "clsx";
-import { ClearPropsContext } from "@/lib/propsContext";
+import {
+  ClearPropsContext,
+  PropsContext,
+  PropsContextProvider,
+} from "@/lib/propsContext";
+import { deepFindOfType } from "@/lib/react/deepFindOfType";
+import { NavigationGroup } from "@/components/Navigation";
+import { Wrap } from "@/components/Wrap";
 
 export interface NavigationProps
   extends PropsWithChildren<ComponentProps<"nav">> {
@@ -13,12 +20,25 @@ export const Navigation: FC<NavigationProps> = (props) => {
 
   const rootClassName = clsx(styles.navigation, className);
 
-  //   const hasGroups = !!deepFindOfType(children, NavigationGroup);
+  const hasGroups = !!deepFindOfType(children, NavigationGroup);
+
+  console.log(hasGroups);
+
+  const propsContext: PropsContext = {
+    Heading: {
+      className: styles.heading,
+      level: 3,
+    },
+  };
 
   return (
     <ClearPropsContext>
       <nav className={rootClassName} role="navigation" {...rest}>
-        <ul>{children}</ul>
+        <PropsContextProvider props={propsContext}>
+          <Wrap if={!hasGroups}>
+            <NavigationGroup>{children}</NavigationGroup>
+          </Wrap>
+        </PropsContextProvider>
       </nav>
     </ClearPropsContext>
   );
--- packages/components/src/components/Navigation/components/NavigationItemGroup/index.ts
+++
@@ -1,6 +0,0 @@
-import { NavigationItemGroup } from "./NavigationItemGroup";
-export {
-  NavigationItemGroup,
-  type NavigationItemGroupProps,
-} from "./NavigationItemGroup";
-export default NavigationItemGroup;
