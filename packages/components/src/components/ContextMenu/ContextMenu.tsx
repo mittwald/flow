71e536a65b45f882a33c8b7180f883b2786d7420
lmeyer <l.meyer@mittwald.de>
2024-03-26 14:36:05 +0100

    fix: fix scrolling and update classname

--- packages/components/src/components/ContextMenu/ContextMenu.tsx
+++ packages/components/src/components/ContextMenu/ContextMenu.tsx
@@ -13,7 +13,7 @@ export const ContextMenu: FC<ContextMenuProps> = (props) => {
 
   return (
     <Aria.Popover className={styles.contextMenu} {...rest}>
-      <Aria.Menu className={styles.menu} onAction={onAction}>
+      <Aria.Menu className={styles.menuList} onAction={onAction}>
         {children}
       </Aria.Menu>
     </Aria.Popover>
