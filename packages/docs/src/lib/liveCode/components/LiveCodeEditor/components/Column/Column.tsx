b2e9eb6f8d8e4c6338ab825e0ba42f1b803926ee
lmeyer <l.meyer@mittwald.de>
2024-04-05 08:37:33 +0200

    feat(BoldTextPlaceholder): Add BoldTextPlaceholder component

--- packages/docs/src/lib/liveCode/components/LiveCodeEditor/components/Column/Column.tsx
+++ packages/docs/src/lib/liveCode/components/LiveCodeEditor/components/Column/Column.tsx
@@ -1,4 +1,4 @@
-import { FC, PropsWithChildren } from "react";
+import type { FC, PropsWithChildren } from "react";
 import styles from "./Column.module.css";
 
 export const Column: FC<PropsWithChildren> = (props) => (
