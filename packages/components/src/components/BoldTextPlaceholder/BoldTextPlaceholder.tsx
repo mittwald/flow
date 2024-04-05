058c820d5b5af01c0cec68d6d88969265df3e3ba
lmeyer <l.meyer@mittwald.de>
2024-04-05 08:27:49 +0200

    feat: add BoldTextPlaceholder

--- packages/components/src/components/BoldTextPlaceholder/BoldTextPlaceholder.tsx
+++ packages/components/src/components/BoldTextPlaceholder/BoldTextPlaceholder.tsx
@@ -1,14 +1,17 @@
 import React, { ComponentProps, FC, PropsWithChildren } from "react";
 import styles from "./BoldTextPlaceholder.module.scss";
+import clsx from "clsx";
 
 export interface BoldTextPlaceholderProps
-  extends PropsWithChildren<ComponentProps<"div">> {}
+  extends PropsWithChildren<ComponentProps<"span">> {}
 
 export const BoldTextPlaceholder: FC<BoldTextPlaceholderProps> = (props) => {
-  const { children } = props;
+  const { children, className, ...rest } = props;
+
+  const rootClassName = clsx(styles.boldTextPlaceholder, className);
 
   return (
-    <span className={styles.boldTextPlaceholder}>
+    <span className={rootClassName} {...rest}>
       {children}
       <span className={styles.boldText}>{children}</span>
     </span>
