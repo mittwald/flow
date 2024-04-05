9d7f8c34309166fd936dfd4211e5c40589a4827b
lmeyer <l.meyer@mittwald.de>
2024-04-05 08:29:23 +0200

    docs: fix column classname

--- packages/docs/src/lib/liveCode/components/LiveCodeEditor/components/Column/Column.tsx
+++ packages/docs/src/lib/liveCode/components/LiveCodeEditor/components/Column/Column.tsx
@@ -2,5 +2,5 @@ import { FC, PropsWithChildren } from "react";
 import styles from "./Column.module.css";
 
 export const Column: FC<PropsWithChildren> = (props) => (
-  <div className={styles.row}>{props.children}</div>
+  <div className={styles.column}>{props.children}</div>
 );
