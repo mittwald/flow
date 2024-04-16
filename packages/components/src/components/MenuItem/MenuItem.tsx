3757a55192c621e5a93de8026b85f79ffa4be659
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-04-16 08:22:08 +0200

    feat(MenuItem): support links in MenuItem

--- packages/components/src/components/MenuItem/MenuItem.tsx
+++ packages/components/src/components/MenuItem/MenuItem.tsx
@@ -1,24 +1,33 @@
-import type { FC, PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 import React from "react";
 import * as Aria from "react-aria-components";
 import styles from "./MenuItem.module.scss";
 import clsx from "clsx";
 import { MenuItemContent } from "@/components/MenuItem/MenuItemContent";
+import type { FlowComponentProps } from "@/lib/componentFactory/flowComponent";
+import { flowComponent } from "@/lib/componentFactory/flowComponent";
 
 export interface MenuItemProps
   extends Omit<Aria.MenuItemProps, "children">,
-    PropsWithChildren {}
+    PropsWithChildren,
+    FlowComponentProps<"MenuItem"> {
+  selectionVariant?: "control" | "navigation";
+}
 
-export const MenuItem: FC<MenuItemProps> = (props) => {
-  const { children, className, ...rest } = props;
+export const MenuItem = flowComponent("MenuItem", (props) => {
+  const { children, className, selectionVariant, ...rest } = props;
 
   const rootClassName = clsx(styles.menuItem, className);
 
   return (
     <Aria.MenuItem {...rest} className={rootClassName}>
-      {(props) => <MenuItemContent {...props}>{children}</MenuItemContent>}
+      {(props) => (
+        <MenuItemContent {...props} selectionVariant={selectionVariant}>
+          {children}
+        </MenuItemContent>
+      )}
     </Aria.MenuItem>
   );
-};
+});
 
 export default MenuItem;
