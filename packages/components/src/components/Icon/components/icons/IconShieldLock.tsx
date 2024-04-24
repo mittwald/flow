ae57bc48f0815d6548f563c98f0382e2a2489267
Mathis Fullriede <m.fullriede@mittwald.de>
2024-04-24 09:46:33 +0200

    feat(icons): add Lock, LockOpen & ShieldLock icons

---
+++ packages/components/src/components/Icon/components/icons/IconShieldLock.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* auto-generated file */
+import React, { ComponentProps, FC } from "react";
+import { IconShieldLock as Tabler } from "@tabler/icons-react";
+import { Icon } from "@/components/Icon";
+
+export const IconShieldLock: FC<
+  Omit<ComponentProps<typeof Icon>, "children">
+> = (props) => (
+  <Icon {...props}>
+    <Tabler />
+  </Icon>
+);
+
+export default IconShieldLock;
