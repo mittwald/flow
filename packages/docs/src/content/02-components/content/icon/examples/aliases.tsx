9d77cd2995b1227144cbcb28617006a02decf386
lmeyer <l.meyer@mittwald.de>
2024-04-29 09:45:13 +0200

    docs(Icon): add icon aliases

---
+++ packages/docs/src/content/02-components/content/icon/examples/aliases.tsx
@@ -0,0 +1,76 @@
+import {
+  IconApp,
+  IconBackLink,
+  IconBackup,
+  IconCheck,
+  IconCheckboxChecked, IconCheckboxEmpty, IconCheckboxIndeterminate, IconChevronDown,
+} from "@mittwald/flow-react-components/Icons";
+
+<Row>
+  <Column><IconApp />App</Column>
+  <Column><IconBackLink />Backlink</Column>
+  <Column><IconBackup />Backup</Column>
+  <Column><IconCheck />Check</Column>
+  <Column><IconCheckboxChecked />Checkbox checked</Column>
+  <Column><IconCheckboxEmpty />Checkbox empty</Column>
+  <Column><IconCheckboxIndeterminate />Checkbox indeterminate</Column>
+  <Column><IconChevronDown />Chevron down</Column>
+  <Column><IconChevronDown />Chevron down</Column>
+</Row>;
+
+
+
+ChevronLeft: ChevronLeft
+ChevronRight: ChevronRight
+ChevronUp: ChevronUp
+Close: X
+Code: Code
+ContextMenu: Dots
+Copy: Copy
+Cronjob: CalendarCheck
+Customer: Building
+Danger: AlertTriangle
+Dashboard: LayoutBoard
+Database: Database
+Date: Calendar
+Delete: Trash
+Domain: World
+Email: Mail
+ExternalLink: ExternalLink
+Failed: X
+Filter: Filter
+Hide: EyeCancel
+Home: Home
+Info: InfoCircle
+Inbox: Inbox
+ListView: List
+Logout: Power
+Member: UsersGroup
+Minus: Minus
+Notification: Bell
+Pending: Loader2
+Plus: Plus
+Project: Archive
+Random: Dice3
+RadioOff: Circle
+RadioOn: CircleDotFilled
+Save: DeviceFloppy
+Search: Search
+SearchEngine: ListSearch
+Server: Server
+Settings: Settings
+Share: Share3
+Show: Eye
+Sorting: AdjustmentsHorizontal
+SpamProtectionOff: Shield
+SpamProtectionOn: ShieldCheckFilled
+SshSftp: Door
+Subdomain: Subtask
+Succeeded: Check
+Success: CircleCheck
+Support: Headset
+Terminate: FileX
+TileView: LayoutGrid
+Time: Clock
+Warning: AlertCircle
+Mittwald:
