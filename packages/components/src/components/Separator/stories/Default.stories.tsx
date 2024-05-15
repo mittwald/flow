8f090e9eb7b532576a0eb8399ba3e0205629896b
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-05-15 14:25:22 +0200

    feat(Separator): add Separator component (#432)

---
+++ packages/components/src/components/Separator/stories/Default.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { Separator } from "@/components/Separator";
+
+const meta: Meta<typeof Separator> = {
+  title: "Structure/Separator",
+  component: Separator,
+  render: (props) => <Separator {...props} />,
+};
+export default meta;
+
+type Story = StoryObj<typeof Separator>;
+
+export const Default: Story = {};
