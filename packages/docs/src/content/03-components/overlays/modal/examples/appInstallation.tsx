670db4bcd7350a600e751baecad1cdcf3f0a988e
lmeyer <l.meyer@mittwald.de>
2024-06-06 08:20:26 +0200

    docs(Modal): add app selection modal

---
+++ packages/docs/src/content/03-components/overlays/modal/examples/appInstallation.tsx
@@ -0,0 +1,82 @@
+import { useOverlayController } from "@mittwald/flow-react-components/controller";
+import Button from "@mittwald/flow-react-components/Button";
+import { Modal } from "@mittwald/flow-react-components/Modal";
+import { Content } from "@mittwald/flow-react-components/Content";
+import { Text } from "@mittwald/flow-react-components/Text";
+import { Initials } from "@mittwald/flow-react-components/Initials";
+import { Avatar } from "@mittwald/flow-react-components/Avatar";
+import List, {
+  ListItem,
+  ListItemView,
+  ListStaticData,
+} from "@mittwald/flow-react-components/List";
+import { Heading } from "@mittwald/flow-react-components/Heading";
+
+export default () => {
+  const appSelectionController = useOverlayController();
+  const configurationController = useOverlayController();
+
+  interface App {
+    name: string;
+    description: string;
+    versions: string;
+  }
+
+  const apps: App[] = [
+    {
+      name: "WordPress",
+      description:
+        "Eins der meist genutzten CMS. Perfekt für Blogs und Websites.",
+      versions: "Versionen X.XX bis X.XX",
+    },
+    {
+      name: "TYPO3",
+      description:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+      versions: "Versionen X.XX bis X.XX",
+    },
+    {
+      name: "Joomla!",
+      description:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+      versions: "Versionen X.XX bis X.XX",
+    },
+  ];
+
+  return (
+    <>
+      <Button
+        color="accent"
+        onPress={appSelectionController.open}
+      >
+        App installieren
+      </Button>
+      <Modal
+        offCanvas
+        controller={appSelectionController}
+        size="l"
+      >
+        <Content>
+          <List batchSize={5}>
+            <ListStaticData data={apps} />
+            <ListItemView<App>>
+              {(app: App) => (
+                <ListItem
+                  onPress={appSelectionController.close}
+                >
+                  <Avatar>
+                    <Initials>app.name</Initials>
+                  </Avatar>
+                  <Heading>{app.name}</Heading>
+                  <Text>{app.versions}</Text>
+                  <Content>{app.description}</Content>
+                </ListItem>
+              )}
+            </ListItemView>
+          </List>
+          ;
+        </Content>
+      </Modal>
+    </>
+  );
+};
