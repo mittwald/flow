74c5a51d84f64a13636dfa0b111b1ad0e0846a1a
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-06-07 11:51:46 +0200

    feat!(Form): show Action state in submit button

--- packages/components/src/integrations/react-hook-form/components/Form/Form.tsx
+++ packages/components/src/integrations/react-hook-form/components/Form/Form.tsx
@@ -1,20 +1,79 @@
-import type { ComponentProps, PropsWithChildren } from "react";
+import type {
+  ComponentProps,
+  FormEventHandler,
+  PropsWithChildren,
+} from "react";
+import { useEffect } from "react";
 import React from "react";
 import type { FieldValues, UseFormReturn } from "react-hook-form";
+import { useFormState } from "react-hook-form";
 import { FormContextProvider } from "@/integrations/react-hook-form/components/context/formContext";
+import { ActionStateContext } from "@/components/Action/ActionStateContext";
+
+export type FormOnSubmitHandler<F extends FieldValues> = Parameters<
+  UseFormReturn<F>["handleSubmit"]
+>[0];
 
 interface Props<F extends FieldValues>
-  extends ComponentProps<"form">,
+  extends Omit<ComponentProps<"form">, "onSubmit">,
     PropsWithChildren {
-  form?: UseFormReturn<F>;
+  form: UseFormReturn<F>;
+  onSubmit: FormOnSubmitHandler<F>;
 }
 
 export function Form<F extends FieldValues>(props: Props<F>) {
-  const { form, children, ...formProps } = props;
+  const { form, children, onSubmit, ...formProps } = props;
+
+  const { isValid, isSubmitted, isSubmitting, isSubmitSuccessful, errors } =
+    useFormState(form);
+
+  const unwatchedFormState = form.control._formState;
+
+  useEffect(() => {
+    if (isSubmitted && isValid) {
+      form?.reset(undefined, {
+        keepIsSubmitted: false,
+        keepIsSubmitSuccessful: false,
+        keepDefaultValues: true,
+        keepValues: true,
+        keepDirtyValues: true,
+        keepIsValid: true,
+        keepDirty: true,
+        keepErrors: true,
+        keepTouched: true,
+        keepIsValidating: true,
+        keepSubmitCount: true,
+      });
+    }
+  }, [isSubmitted, isValid]);
+
+  const handleSubmit = form?.handleSubmit(onSubmit);
+
+  const handleOnSubmit: FormEventHandler = (e) => {
+    if (unwatchedFormState.isSubmitting || unwatchedFormState.isValidating) {
+      e.preventDefault();
+      return false;
+    }
+
+    if (handleSubmit) {
+      return handleSubmit(e);
+    }
+  };
+
+  const submitError = isSubmitted ? errors : undefined;
+  const submitSucceeded = isSubmitted && isSubmitSuccessful;
 
   return (
     <FormContextProvider value={{ form }}>
-      <form {...formProps}>{children}</form>
+      <form {...formProps} onSubmit={handleOnSubmit}>
+        <ActionStateContext
+          isStarted={isSubmitting}
+          hasFailedWithError={submitError}
+          hasSucceeded={submitSucceeded}
+        >
+          {children}
+        </ActionStateContext>
+      </form>
     </FormContextProvider>
   );
 }
