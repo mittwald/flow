b1149f3383d9d5fec69e10faf88163173e9feb19
lmeyer <l.meyer@mittwald.de>
2024-07-03 07:55:31 +0200

    docs(FileTrigger): add examples for FileTrigger

---
+++ packages/docs/src/content/03-components/upload/file-trigger/examples/multiple.tsx
@@ -0,0 +1,29 @@
+import FileTrigger, {
+  useFileController,
+} from "@mittwald/flow-react-components/FileTrigger";
+import Button from "@mittwald/flow-react-components/Button";
+import Section from "@mittwald/flow-react-components/Section";
+import LabeledValue from "@mittwald/flow-react-components/LabeledValue";
+import Label from "@mittwald/flow-react-components/Label";
+import Text from "@mittwald/flow-react-components/Text";
+
+export default () => {
+  const controller = useFileController();
+  const files = controller.useFiles();
+
+  return (
+    <Section>
+      <FileTrigger controller={controller}>
+        <Button>Dateien auswählen</Button>
+      </FileTrigger>
+      <LabeledValue>
+        <Label>Ausgewählte Dateien</Label>
+        <Text>
+          {files.length > 0
+            ? files.map((f) => f.name).join(", ")
+            : "-"}
+        </Text>
+      </LabeledValue>
+    </Section>
+  );
+};
