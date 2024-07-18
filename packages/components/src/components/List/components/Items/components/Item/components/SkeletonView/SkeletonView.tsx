d0c02ef5446364fe50080a9f46f8477425fd547f
Tim Auf dem Kampe <t.aufdemkampe@mittwald.de>
2024-07-18 08:05:38 +0200

    feat(SkeletonText): Add SkeletonText component and use it in List SkeletonView

--- packages/components/src/components/List/components/Items/components/Item/components/SkeletonView/SkeletonView.tsx
+++ packages/components/src/components/List/components/Items/components/Item/components/SkeletonView/SkeletonView.tsx
@@ -1,17 +1,17 @@
-import { Skeleton } from "@/components/Skeleton";
 import type { FC } from "react";
 import React from "react";
 import { Heading } from "@/components/Heading";
 import { Text } from "@/components/Text";
 import View from "@/components/List/components/Items/components/Item/components/View/View";
+import SkeletonText from "@/components/SkeletonText";
 
 export const SkeletonView: FC = () => (
   <View>
     <Heading>
-      <Skeleton width="200px" />
+      <SkeletonText width="200px" />
     </Heading>
     <Text>
-      <Skeleton width="300px" />
+      <SkeletonText width="300px" />
     </Text>
   </View>
 );
