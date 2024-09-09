2ecb36b5366c720097f0b805ccb1ac02b75c93dc
lmeyer <l.meyer@mittwald.de>
2024-09-09 08:58:31 +0200

    fix(Popover): fix popover trigger

--- packages/components/src/components/Popover/components/PopoverTrigger/PopoverTrigger.tsx
+++ packages/components/src/components/Popover/components/PopoverTrigger/PopoverTrigger.tsx
@@ -3,18 +3,22 @@ import type { FC } from "react";
 import React from "react";
 import type { OverlayTriggerProps } from "@/components/OverlayTrigger";
 import { OverlayTrigger } from "@/components/OverlayTrigger";
+import { flowComponent } from "@/lib/componentFactory/flowComponent";
 
 type Props = Omit<OverlayTriggerProps, "overlayType">;
 
-export const PopoverTrigger: FC<Props> = (props) => {
-  const { children, ...triggerProps } = props;
-  return (
-    <OverlayTrigger
-      overlayType="Popover"
-      {...triggerProps}
-      component={Aria.DialogTrigger}
-    >
-      {children}
-    </OverlayTrigger>
-  );
-};
+export const PopoverTrigger: FC<Props> = flowComponent(
+  "PopoverTrigger",
+  (props) => {
+    const { children, ...triggerProps } = props;
+    return (
+      <OverlayTrigger
+        overlayType="Popover"
+        {...triggerProps}
+        component={Aria.DialogTrigger}
+      >
+        {children}
+      </OverlayTrigger>
+    );
+  },
+);
