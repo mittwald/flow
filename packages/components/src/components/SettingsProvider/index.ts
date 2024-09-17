412a6cdf267fb3c15436fb12283630834b40d280
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-09-17 09:07:52 +0200

    fix: fix export statement

--- packages/components/src/components/SettingsProvider/index.ts
+++ packages/components/src/components/SettingsProvider/index.ts
@@ -1,6 +1,6 @@
-export { SettingsJson } from "@/components/SettingsProvider/models/SettingsStore";
 export { SettingsProvider } from "@/components/SettingsProvider/SettingsProvider";
 export type { SettingsBackend } from "@/components/SettingsProvider/backends/types";
+export type { SettingsJson } from "@/components/SettingsProvider/models/SettingsStore";
 
 import { SettingsProvider } from "@/components/SettingsProvider/SettingsProvider";
 export default SettingsProvider;
