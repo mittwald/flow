d7d01e6663c5560fcd69c665f9fca311a21a7556
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-09-17 21:48:45 +0200

    fix(SettingProvider): refresh settings after update

--- packages/components/src/components/SettingsProvider/SettingsProvider.tsx
+++ packages/components/src/components/SettingsProvider/SettingsProvider.tsx
@@ -8,7 +8,7 @@ import React, {
 } from "react";
 import type { SupportedSettingsBackend } from "@/components/SettingsProvider/backends/types";
 import { autorun } from "mobx";
-import { usePromise } from "@mittwald/react-use-promise";
+import { getAsyncResource } from "@mittwald/react-use-promise";
 import { SettingsStore } from "@/components/SettingsProvider/models/SettingsStore";
 import { settingsBackendFactory } from "@/components/SettingsProvider/backends/settingsBackendFactory";
 
@@ -25,26 +25,26 @@ export const SettingsProvider: FC<Props> = (props) => {
   const { children, id, ...storeShape } = props;
   const backend = settingsBackendFactory(storeShape);
 
-  const storingPromise = useRef(Promise.resolve());
-  const storedSettings = usePromise(() => backend.load(), [], {
+  const storedSettingsResource = getAsyncResource(() => backend.load(), [], {
     loaderId: id,
   });
+  const storedSettings = storedSettingsResource.use();
+  const storingPromise = useRef(Promise.resolve());
 
   const settingsStore = useMemo(
     () => SettingsStore.fromJson(storedSettings),
     [id],
   );
 
-  useEffect(
-    () =>
-      autorun(() => {
-        const json = settingsStore.asJson;
-        storingPromise.current = storingPromise.current.then(() =>
-          backend.store(json),
-        );
-      }),
-    [id],
-  );
+  const watchAndStoreSettings = () => {
+    const json = settingsStore.asJson;
+    storingPromise.current = storingPromise.current.then(async () => {
+      await backend.store(json);
+      storedSettingsResource.refresh();
+    });
+  };
+
+  useEffect(() => autorun(watchAndStoreSettings), [id]);
 
   return <context.Provider value={settingsStore}>{children}</context.Provider>;
 };
