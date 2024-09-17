ab7456a49bfe14994d97a5e1aaec24b3c6b78717
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-09-17 09:01:47 +0200

    feat(List): support multiple SettingProvider

--- packages/components/src/components/SettingsProvider/SettingsProvider.tsx
+++ packages/components/src/components/SettingsProvider/SettingsProvider.tsx
@@ -12,29 +12,40 @@ import { usePromise } from "@mittwald/react-use-promise";
 import { SettingsStore } from "@/components/SettingsProvider/models/SettingsStore";
 import { settingsBackendFactory } from "@/components/SettingsProvider/backends/settingsBackendFactory";
 
-type Props = PropsWithChildren & SupportedSettingsBackend;
+type Props = PropsWithChildren &
+  SupportedSettingsBackend & {
+    id?: string;
+  };
 
 const context = createContext<SettingsStore | undefined>(undefined);
 
 export const useSettings = () => useContext(context);
 
 export const SettingsProvider: FC<Props> = (props) => {
-  const { children, ...storeShape } = props;
+  const { children, id, ...storeShape } = props;
   const store = settingsBackendFactory(storeShape);
 
+  const firstAutorun = useRef(true);
   const storingPromise = useRef(Promise.resolve());
-  const storedSettings = usePromise(() => store.load(), []);
+  const storedSettings = usePromise(() => store.load(), [], {
+    loaderId: id,
+  });
   const settings = useMemo(() => SettingsStore.fromJson(storedSettings), []);
 
   useEffect(
     () =>
       autorun(() => {
+        // skip initial autorun
+        if (firstAutorun.current) {
+          firstAutorun.current = false;
+          return;
+        }
         const asJson = settings.asJson;
         storingPromise.current = storingPromise.current.then(() =>
           store.store(asJson),
         );
       }),
-    [settings],
+    [settings, id],
   );
 
   return <context.Provider value={settings}>{children}</context.Provider>;
--- packages/components/src/components/SettingsProvider/index.ts
+++ packages/components/src/components/SettingsProvider/index.ts
@@ -1,5 +1,6 @@
-import { SettingsProvider } from "@/components/SettingsProvider/SettingsProvider";
+export { SettingsJson } from "@/components/SettingsProvider/models/SettingsStore";
 export { SettingsProvider } from "@/components/SettingsProvider/SettingsProvider";
 export type { SettingsBackend } from "@/components/SettingsProvider/backends/types";
 
+import { SettingsProvider } from "@/components/SettingsProvider/SettingsProvider";
 export default SettingsProvider;
