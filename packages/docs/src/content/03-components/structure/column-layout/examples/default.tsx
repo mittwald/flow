296d9955432fa5bfbb4809e58d03f5a3662afae8
Marie Jünke <m.juenke@mittwald.de>
2024-09-27 11:11:45 +0200

    adjust examples

--- packages/docs/src/content/03-components/structure/column-layout/examples/default.tsx
+++ packages/docs/src/content/03-components/structure/column-layout/examples/default.tsx
@@ -2,22 +2,17 @@ import ColumnLayout from "@mittwald/flow-react-components/ColumnLayout";
 import TextField from "@mittwald/flow-react-components/TextField";
 import Label from "@mittwald/flow-react-components/Label";
 
-<ColumnLayout m={[1, 1]} rowGap="s" columnGap="l">
+<ColumnLayout rowGap="s" columnGap="l">
   <TextField>
     <Label>Vorname</Label>
   </TextField>
   <TextField>
     <Label>Nachname</Label>
   </TextField>
-  <ColumnLayout s={[2, 1]} columnGap="m">
-    <TextField>
-      <Label>Straße</Label>
-    </TextField>
-    <TextField>
-      <Label>Hausnummer</Label>
-    </TextField>
-  </ColumnLayout>
   <TextField>
-    <Label>Ort</Label>
+    <Label>Straße</Label>
+  </TextField>
+  <TextField>
+    <Label>Hausnummer</Label>
   </TextField>
 </ColumnLayout>;
