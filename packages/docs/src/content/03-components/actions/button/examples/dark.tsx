1900e0724b4d8950b51d71bce5a82129320bae57
lmeyer <l.meyer@mittwald.de>
2024-10-01 09:11:18 +0200

    docs: build docs

--- packages/docs/src/content/03-components/actions/button/examples/dark.tsx
+++ packages/docs/src/content/03-components/actions/button/examples/dark.tsx
@@ -8,7 +8,6 @@ import Button from "@mittwald/flow-react-components/Button";
   <Button variant="plain" color="dark">
     Dark
   </Button>
-
   <Button variant="outline" color="dark">
     Dark
   </Button>
