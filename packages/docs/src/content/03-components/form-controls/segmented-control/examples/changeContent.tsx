39484961b9cf98433e7ca7340b299efa58366a70
Marie Jünke <m.juenke@mittwald.de>
2024-10-10 14:49:50 +0200

    fix change content example

--- packages/docs/src/content/03-components/form-controls/segmented-control/examples/changeContent.tsx
+++ packages/docs/src/content/03-components/form-controls/segmented-control/examples/changeContent.tsx
@@ -8,29 +8,36 @@ import { FieldDescription } from "@mittwald/flow-react-components/FieldDescripti
 import { ColumnLayout } from "@mittwald/flow-react-components/ColumnLayout";
 import { TextField } from "@mittwald/flow-react-components/TextField";
 
-const [showContent, setShowContent] = useState(false);
+export default () => {
+  const [showContent, setShowContent] =
+    useState<boolean>(false);
 
-<RadioGroup
-  variant="segmented"
-  defaultValue="lastschrift"
-  s={[1, 1]}
-  onChange={() => setShowContent(!showContent)}
->
-  <Label>Zahlungsart</Label>
-  <Radio value="lastschrift">Lastschrift</Radio>
-  <Radio value="Rechnung">Rechnung</Radio>
-  {showContent && (
+  return (
     <>
-      <FieldDescription>
-        Wähle bitte eine Bankverbindung für die Bezahlung
-        mit SEPA-Lastschrift aus.
-      </FieldDescription>
-      <ColumnLayout>
-        <TextField isRequired>
-          <Label>Kontoinhaber</Label>
-        </TextField>
-        <TextField isRequired>IBAN</TextField>
-      </ColumnLayout>
+      <RadioGroup
+        variant="segmented"
+        defaultValue="lastschrift"
+        s={[1, 1]}
+        onChange={() => setShowContent(!showContent)}
+      >
+        <Label>Zahlungsart</Label>
+        <Radio value="lastschrift">Lastschrift</Radio>
+        <Radio value="Rechnung">Rechnung</Radio>
+        {showContent && (
+          <FieldDescription>
+            Wähle bitte eine Bankverbindung für die
+            Bezahlung mit SEPA-Lastschrift aus.
+          </FieldDescription>
+        )}
+      </RadioGroup>
+      {showContent && (
+        <ColumnLayout>
+          <TextField isRequired>
+            <Label>Kontoinhaber</Label>
+          </TextField>
+          <TextField isRequired>IBAN</TextField>
+        </ColumnLayout>
+      )}
     </>
-  )}
-</RadioGroup>;
+  );
+};
