05437591d71134e87694306c30571aab5b04299f
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-11-07 13:33:34 +0100

    feat(FileCard): Add Link and Image to FileCard (#957)

---
+++ packages/components/src/components/FileCard/components/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,31 @@
+import type { FC } from "react";
+import React from "react";
+import { Button } from "@/components/Button";
+import styles from "@/components/FileCard/FileCard.module.scss";
+import { IconClose } from "@/components/Icon/components/icons";
+import locales from "../../locales/*.locale.json";
+import { useLocalizedStringFormatter } from "react-aria";
+
+interface Props {
+  onDelete: () => void;
+}
+
+export const DeleteButton: FC<Props> = (props) => {
+  const { onDelete } = props;
+  const stringFormatter = useLocalizedStringFormatter(locales);
+
+  return (
+    <Button
+      className={styles.deleteButton}
+      aria-label={stringFormatter.format(`fileCard.delete`)}
+      size="s"
+      variant="plain"
+      color="secondary"
+      onPress={onDelete}
+    >
+      <IconClose />
+    </Button>
+  );
+};
+
+export default DeleteButton;
