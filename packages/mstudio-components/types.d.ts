024b6d3f0f50d01dce80e6801736f423bc0b6448
lmeyer <l.meyer@mittwald.de>
2024-11-11 10:35:43 +0100

    feat(UserMenu): add user menu

--- packages/mstudio-components/types.d.ts
+++ packages/mstudio-components/types.d.ts
@@ -4,9 +4,3 @@ declare module "*.module.css" {
   const classes: Record<string, string>;
   export default classes;
 }
-
-declare module "*.locale.json" {
-  import type { LocalizedStrings } from "react-aria";
-  const langFile: LocalizedStrings;
-  export default langFile;
-}
