a32d76747b075e6058701d1d9a7c491da0aa8a80
lmeyer <l.meyer@mittwald.de>
2024-11-11 09:09:16 +0100

    docs: add code template examples

--- packages/code-template/app/_components/DetailedInformation/DetailedInformation.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC, PropsWithChildren } from "react";
-import styles from "./DetailedInformation.module.scss";
-
-export const DetailedInformation: FC<PropsWithChildren> = (props) => {
-  const { children } = props;
-
-  return <div className={styles.detailedInformation}>{children}</div>;
-};
