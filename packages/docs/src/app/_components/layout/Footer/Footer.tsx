1360640ce50d0800594695dbd948de79da6d4dc4
Marie Jünke <m.juenke@mittwald.de>
2024-11-14 14:45:35 +0100

    add svgs and styling

--- packages/docs/src/app/_components/layout/Footer/Footer.tsx
+++ packages/docs/src/app/_components/layout/Footer/Footer.tsx
@@ -1,48 +1,66 @@
 "use client";
 import type { FC } from "react";
+import React from "react";
 import { LayoutCard } from "@mittwald/flow-react-components/LayoutCard";
 import { ColumnLayout } from "@mittwald/flow-react-components/ColumnLayout";
 import { Section } from "@mittwald/flow-react-components/Section";
 import { Heading } from "@mittwald/flow-react-components/Heading";
 import { Header } from "@mittwald/flow-react-components/Header";
 import { Link } from "@mittwald/flow-react-components/Link";
+import styles from "./footer.module.scss";
+import { Image } from "@mittwald/flow-react-components/Image";
+import feedback from "../../../../../assets/Styleguide-Footer_Feedback.svg";
+import logoMittwald from "../../../../../assets/mittwald-logo-footer.svg";
+import clsx from "clsx";
 
 export const Footer: FC = () => {
-  const bildschirmGROSS = true;
-
   return (
-    <footer>
-      <LayoutCard>
-        <ColumnLayout m={[1, 1, 2]} l={[1, 1, 3]}>
-          <Section>
-            <Header>
-              <Heading level={3}>Contributoren-Seiten</Heading>
-            </Header>
-            <Link href="https://github.com/mittwald/flow">GitHub Repo</Link>
-            <Link href="https://developer.mittwald.de/">Developer-Portal</Link>
-          </Section>
-          <Section>
-            <Header>
-              <Heading level={3}>Rechtliches</Heading>
-            </Header>
-            <Link href="https://www.mittwald.de/impressum">Impressum</Link>
-            <Link href="https://www.mittwald.de/datenschutz">Datenschutz</Link>
-          </Section>
-          <>
-            <ColumnLayout m={[1, 1]}>
-              <Section>
-                <Heading level={3}>
-                  Hilf uns, Flow noch besser zu machen!
-                </Heading>
-                Fehlt dir eine bestimmte Component oder etwas Anderes? Hast du
-                Feedback? Dann teile es uns gerne auf GitHub mit.
-                <Link href="#">Feedback zu Flow geben</Link>
-              </Section>
-              {bildschirmGROSS && <div>Hier kommt Bild und so</div>}
-            </ColumnLayout>
-          </>
-        </ColumnLayout>
-      </LayoutCard>
-    </footer>
+    <LayoutCard elementType="footer" className={styles.footer}>
+      <ColumnLayout m={[1, 1, 2]} l={[1, 1, 3]}>
+        <Section>
+          <Header>
+            <Heading level={3}>Contributoren-Seiten</Heading>
+          </Header>
+          <Link color="dark" href="https://github.com/mittwald/flow">
+            GitHub Repo
+          </Link>
+          <Link color="dark" href="https://developer.mittwald.de/">
+            Developer-Portal
+          </Link>
+        </Section>
+        <Section>
+          <Header>
+            <Heading level={3}>Rechtliches</Heading>
+          </Header>
+          <Link color="dark" href="https://www.mittwald.de/impressum">
+            Impressum
+          </Link>
+          <Link color="dark" href="https://www.mittwald.de/datenschutz">
+            Datenschutz
+          </Link>
+        </Section>
+        <>
+          <ColumnLayout m={[1]} l={[1, 1]}>
+            <Section>
+              <Heading level={3}>Hilf uns, Flow noch besser zu machen!</Heading>
+              Fehlt dir eine bestimmte Component oder etwas Anderes? Hast du
+              Feedback? Dann teile es uns gerne auf GitHub mit.
+              <Link color="dark" href="#">
+                Feedback zu Flow geben
+              </Link>
+            </Section>
+            <Image
+              src={feedback.src}
+              alt="Styleguide Feedback"
+              className={clsx(styles.feedbackPicture)}
+            />
+          </ColumnLayout>
+        </>
+      </ColumnLayout>
+      <small>
+        <Image src={logoMittwald.src} alt="mittwald Logo" />© 2024 Mittwald CM
+        Service GmbH & Co. KG
+      </small>
+    </LayoutCard>
   );
 };
