dcecea54bafde3ef6812d2c62ac78875d4fbc32f
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-11-28 07:59:50 +0100

    feat(Message): add message component (#1004)

--- packages/components/src/components/Align/Align.tsx
+++ packages/components/src/components/Align/Align.tsx
@@ -1,14 +1,19 @@
-import type { FC, PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 import React from "react";
 import styles from "./Align.module.scss";
 import clsx from "clsx";
 import type { PropsContext } from "@/lib/propsContext";
 import PropsContextProvider from "@/lib/propsContext/PropsContextProvider";
 import type { PropsWithClassName } from "@/lib/types/props";
+import type { FlowComponentProps } from "@/lib/componentFactory/flowComponent";
+import { flowComponent } from "@/lib/componentFactory/flowComponent";
 
-export interface AlignProps extends PropsWithChildren, PropsWithClassName {}
+export interface AlignProps
+  extends PropsWithChildren,
+    PropsWithClassName,
+    FlowComponentProps {}
 
-export const Align: FC<AlignProps> = (props) => {
+export const Align = flowComponent("Align", (props) => {
   const { children, className } = props;
 
   const rootClassName = clsx(styles.align, className);
@@ -23,6 +28,6 @@ export const Align: FC<AlignProps> = (props) => {
       <div className={rootClassName}>{children}</div>
     </PropsContextProvider>
   );
-};
+});
 
 export default Align;
---
+++ packages/components/src/components/Message/stories/Default.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { Message } from "@/components/Message";
+import { Header } from "@/components/Header";
+import { ContextMenu, ContextMenuTrigger } from "@/components/ContextMenu";
+import { Button } from "@/components/Button";
+import MenuItem from "@/components/MenuItem";
+import { Align } from "@/components/Align";
+import { Avatar } from "@/components/Avatar";
+import { Initials } from "@/components/Initials";
+import { Content } from "@/components/Content";
+import { Text } from "@/components/Text";
+
+const meta: Meta<typeof Message> = {
+  title: "Content/Message",
+  component: Message,
+  parameters: {
+    controls: { exclude: ["className"] },
+  },
+  render: (props) => (
+    <Message {...props}>
+      <Header>
+        <ContextMenuTrigger>
+          <Button />
+          <ContextMenu>
+            <MenuItem>Bearbeiten</MenuItem>
+            <MenuItem>Löschen</MenuItem>
+          </ContextMenu>
+        </ContextMenuTrigger>
+        <Align>
+          <Avatar>
+            <Initials>Max Mustermann</Initials>
+          </Avatar>
+          <Text>
+            <b>Max Mustermann</b>
+            Organisationsinhaber
+          </Text>
+        </Align>
+        <Text>01.09.2024, 12:45</Text>
+      </Header>
+
+      <Content>
+        <Text>Das ist eine Nachricht</Text>
+      </Content>
+    </Message>
+  ),
+};
+export default meta;
+
+type Story = StoryObj<typeof Message>;
+
+export const Default: Story = {};
+
+export const Responder: Story = { args: { type: "responder" } };
+
+export const MessageOnly: Story = {
+  render: (props) => (
+    <Message {...props}>
+      <Content>
+        <Text>Das ist eine Nachricht</Text>
+      </Content>
+    </Message>
+  ),
+};
+
+export const OrientationRight: Story = { args: { orientation: "right" } };
