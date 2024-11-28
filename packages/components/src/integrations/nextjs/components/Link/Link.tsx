bd159f8f26bcf8eddc46651d0b95b724ab8932b0
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2024-11-28 12:47:04 +0100

    Update tokens (#985)

--- packages/components/src/integrations/nextjs/components/Link/Link.tsx
+++ packages/components/src/integrations/nextjs/components/Link/Link.tsx
@@ -10,6 +10,8 @@ export const Link = forwardRef<HTMLAnchorElement, Props>(
   function Link(props, ref) {
     const { href, isDisabled, ...rest } = props;
 
+    console.log(props);
+
     return (
       <NextLink
         href={href ?? "#"}
