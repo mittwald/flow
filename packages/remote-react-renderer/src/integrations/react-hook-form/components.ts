7126bb90a28eee0b6701616b08a51b57b712c303
Marco Rieger <m.rieger@mittwald.de>
2024-12-03 16:56:22 +0100

    temp remove

--- packages/remote-react-renderer/src/integrations/react-hook-form/components.ts
+++ packages/remote-react-renderer/src/integrations/react-hook-form/components.ts
@@ -3,6 +3,6 @@ import type { RemoteComponentsMap } from "@/lib/types";
 import type { FlowRemoteRhfElementKeys } from "@mittwald/flow-remote-elements/react-hook-form";
 import { Form } from "@mittwald/flow-react-components/react-hook-form";
 
-export const components: RemoteComponentsMap<FlowRemoteRhfElementKeys> = {
-  "flr.rhf-form": createFlowRemoteComponentRenderer(Form as never),
-};
+// export const components: RemoteComponentsMap<FlowRemoteRhfElementKeys> = {
+//   "flr.rhf-form": createFlowRemoteComponentRenderer(Form as never),
+// };
