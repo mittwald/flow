edd2bb42472a5f70443955ec4e489bf85b548607
Marco Falkenberg <m.falkenberg@mittwald.de>
2024-12-06 09:09:24 +0100

    add svg element example

--- demos/remote-dom/src/app/remote/svg/page.tsx
+++ demos/remote-dom/src/app/remote/svg/page.tsx
@@ -14,15 +14,26 @@ const svgString = `\
     <circle cx="50" cy="50" r="50" />
 </svg>`;
 
+// const svgElement = (
+//   <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+//     <circle cx="50" cy="50" r="50" />
+//   </svg>
+// );
+
 export default function Page() {
   return (
     <Section>
       <Heading level={4}>Icon component</Heading>
       <AlertIcon status="success" />
+
       {/*<Heading level={4}>Tabler icon</Heading>*/}
       {/*<Icon>*/}
       {/*  <IconCircleCheck />*/}
       {/*</Icon>*/}
+
+      {/*<Heading level={4}>SVG element</Heading>*/}
+      {/*<Icon>{svgElement}</Icon>*/}
+
       <Heading level={4}>SVG-String</Heading>
       <Icon>{svgString}</Icon>
     </Section>
