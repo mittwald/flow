ca823296a9b23ccafc6a6fa724290d0d3de4cfa7
lmeyer <l.meyer@mittwald.de>
2024-12-06 08:37:46 +0100

    feat(Icon): add autoresponder icon

---
+++ packages/components/src/components/Icon/components/icons/IconAutoresponder.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* auto-generated file */
+import React, { ComponentProps, FC } from "react";
+import { IconArrowBackUpDouble as Tabler } from "@tabler/icons-react";
+import { Icon } from "@/components/Icon";
+
+export const IconAutoresponder: FC<
+  Omit<ComponentProps<typeof Icon>, "children">
+> = (props) => (
+  <Icon {...props}>
+    <Tabler />
+  </Icon>
+);
+
+export default IconAutoresponder;
