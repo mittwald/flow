8b105f654095080729a2489fe2cf95b3c2618724
lmeyer <l.meyer@mittwald.de>
2024-12-18 10:16:15 +0100

    fix(Table): use th for row header

--- packages/components/src/components/Table/components/TableCell/TableCell.tsx
+++ packages/components/src/components/Table/components/TableCell/TableCell.tsx
@@ -4,9 +4,10 @@ import * as Aria from "react-aria-components";
 import clsx from "clsx";
 import styles from "../../Table.module.scss";
 import { SkeletonText } from "@/components/SkeletonText";
+import { TableColumn } from "@/components/Table";
 
 export interface TableCellProps
-  extends Omit<Aria.CellProps, "children">,
+  extends Omit<Aria.CellProps, "children" | "style">,
     PropsWithChildren {
   rowHeader?: boolean;
 }
@@ -14,15 +15,23 @@ export interface TableCellProps
 export const TableCell: FC<TableCellProps> = (props) => {
   const { children, className, rowHeader, ...rest } = props;
 
-  const rootClassName = clsx(
-    styles.cell,
-    rowHeader && styles.rowHeader,
-    className,
+  const rootClassName = clsx(styles.cell, className);
+
+  const content = (
+    <Suspense fallback={<SkeletonText width="100px" />}>{children}</Suspense>
   );
+
+  if (rowHeader) {
+    return (
+      <TableColumn className={rootClassName} {...rest}>
+        {content}
+      </TableColumn>
+    );
+  }
 
   return (
     <Aria.Cell className={rootClassName} {...rest}>
-      <Suspense fallback={<SkeletonText width="100px" />}>{children}</Suspense>
+      {content}
     </Aria.Cell>
   );
 };
