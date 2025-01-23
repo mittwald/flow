8d52eb10379ec95c5cfa4eecd0474c791a8d9041
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-01-23 13:23:39 +0100

    fix: fix exports

--- packages/remote-react-components/src/components/index.ts
+++ packages/remote-react-components/src/components/index.ts
@@ -2,5 +2,13 @@ export * from "./List";
 export * from "./Form";
 export * from "./Root";
 
-export * from "@mittwald/flow-react-components/Wrap";
-export * from "@mittwald/flow-react-components/Action";
+/**
+ * Using named re-exports here (and not "export * from ..."), because of Next.JS
+ * issue: It's currently unsupported to use "export *" in a client boundary
+ */
+export { Wrap } from "@mittwald/flow-react-components/Wrap";
+export {
+  Action,
+  type ActionFn,
+  type ActionProps,
+} from "@mittwald/flow-react-components/Action";
