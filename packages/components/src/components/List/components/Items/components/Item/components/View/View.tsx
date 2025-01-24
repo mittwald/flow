362f347dd7939ec9c80edaab5503558f926d0b78
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-01-24 14:37:32 +0100

    feat(List): prevent list action in bottom content

--- packages/components/src/components/List/components/Items/components/Item/components/View/View.tsx
+++ packages/components/src/components/List/components/Items/components/Item/components/View/View.tsx
@@ -1,4 +1,4 @@
-import type { PropsWithChildren } from "react";
+import type { ComponentProps, PropsWithChildren } from "react";
 import React from "react";
 import styles from "./View.module.scss";
 import type { PropsContext } from "@/lib/propsContext";
@@ -10,15 +10,19 @@ import clsx from "clsx";
 
 type Props = PropsWithChildren & PropsWithClassName;
 
-const getStyleForContentSlot = (slot?: string) =>
-  slot === "top"
-    ? styles.topContent
-    : slot === "bottom"
-      ? styles.bottomContent
-      : styles.topContent;
-
 export const View = (props: Props) => {
   const { children, className } = props;
+
+  const contentProps: Record<string, ComponentProps<"div">> = {
+    bottom: {
+      onMouseDown: (e) => e.stopPropagation(),
+      onPointerDown: (e) => e.stopPropagation(),
+      className: styles.bottomContent,
+    },
+    top: {
+      className: styles.topContent,
+    },
+  };
 
   const propsContext: PropsContext = {
     ContextMenu: {
@@ -39,7 +43,11 @@ export const View = (props: Props) => {
       },
     },
     Content: {
-      className: dynamic((p) => getStyleForContentSlot(p.slot)),
+      className: dynamic((p) => contentProps[p.slot ?? "top"].className),
+      onMouseDown: dynamic((p) => contentProps[p.slot ?? "top"].onMouseDown),
+      onPointerDown: dynamic(
+        (p) => contentProps[p.slot ?? "top"].onPointerDown,
+      ),
     },
     Avatar: {
       className: styles.avatar,
