a5632d09bdd70524025727ff146d8047cb6c0361
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2025-02-05 10:34:20 +0100

    feat(Align): support icon + text (#1154)

---
+++ packages/docs/src/content/03-components/structure/align/examples/icon-text.tsx
@@ -0,0 +1,8 @@
+import { Align } from "@mittwald/flow-react-components/Align";
+import { Text } from "@mittwald/flow-react-components/Text";
+import { IconDomain } from "@mittwald/flow-react-components/Icons";
+
+<Align>
+  <IconDomain />
+  <Text>mail.agenturserver.de</Text>
+</Align>;
