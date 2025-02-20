b0c374ed0ef07044c2ca1219096d091ca37549d0
lmeyer <l.meyer@mittwald.de>
2025-02-20 10:38:10 +0100

    fix(AvatarStack): add generated files

---
+++ packages/remote-react-components/src/auto-generated/AvatarStack.ts
@@ -0,0 +1,23 @@
+/* prettier-ignore */
+/* This file is auto-generated with the remote-components-generator */
+"use client";
+import createFlowRemoteComponent from "@/components/createFlowRemoteComponent";
+import { RemoteAvatarStackElement } from "@mittwald/flow-remote-elements";
+export { type RemoteAvatarStackElement } from "@mittwald/flow-remote-elements";
+
+export const AvatarStack = createFlowRemoteComponent(
+  "flr-avatar-stack",
+  "AvatarStack",
+  {
+    clearPropsContext: false,
+  },
+  RemoteAvatarStackElement,
+  {
+    slotProps: {
+      wrapper: false,
+    },
+    eventProps: {
+      onCountPress: { event: "countPress" } as never,
+    },
+  },
+);
