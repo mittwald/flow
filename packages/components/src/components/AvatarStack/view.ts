12e3b8ff1fb68727d89debae9de6a66100496d98
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2025-02-20 13:02:18 +0100

    fix(AvatarStack): add generated files (#1201)

---
+++ packages/components/src/components/AvatarStack/view.ts
@@ -0,0 +1,10 @@
+/* prettier-ignore */
+/* This file is auto-generated with the remote-components-generator */
+import type { AvatarStack } from "./AvatarStack";
+import type { ViewComponent } from "@/lib/viewComponentContext";
+
+declare global {
+  interface FlowViewComponents {
+    AvatarStack: ViewComponent<typeof AvatarStack>;
+  }
+}
