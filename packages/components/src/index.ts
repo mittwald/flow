e0fd45880fcaf6815d6c2728120b0472d0872134
JLampeMW <j.lampe@mittwald.de>
2025-03-04 07:27:00 +0100

    feat(General): add missing export of PropsContextProvider (#1232)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,4 @@
 export * from "@/components/public";
-export * from "@/lib/hooks/public";
 export * from "@/lib/controller/public";
+export * from "@/lib/hooks/public";
+export * from "@/lib/propsContext/index";
