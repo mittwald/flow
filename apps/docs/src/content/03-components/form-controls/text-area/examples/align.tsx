457833cfa138d824822fc1f536b0bedf737e619d
lmeyer <l.meyer@mittwald.de>
2025-03-07 08:35:39 +0100

    docs: add align documentation

---
+++ apps/docs/src/content/03-components/form-controls/text-area/examples/align.tsx
@@ -0,0 +1,13 @@
+import {
+  Align,
+  Button,
+  Label,
+  TextArea,
+} from "@mittwald/flow-react-components";
+
+<Align>
+  <TextArea>
+    <Label>Nachricht</Label>
+  </TextArea>
+  <Button>Senden</Button>
+</Align>;
