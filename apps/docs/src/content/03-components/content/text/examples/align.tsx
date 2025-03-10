98f6ce9dda2786c8b361e3a94adc381ba10b90c5
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2025-03-10 09:20:58 +0100

    docs: add align documentation (#1239)

---
+++ apps/docs/src/content/03-components/content/text/examples/align.tsx
@@ -0,0 +1,34 @@
+import {
+  Align,
+  Button,
+  ContextualHelp,
+  ContextualHelpTrigger,
+  CopyButton,
+  IconDomain,
+  Text,
+} from "@mittwald/flow-react-components";
+
+<Column>
+  <Align>
+    <IconDomain />
+    <Text>mail.agenturserver.de</Text>
+  </Align>
+
+  <Align>
+    <Text>mail.agenturserver.de</Text>
+    <CopyButton text="mail.agenturserver.de" />
+  </Align>
+
+  <Align>
+    <Text>mail.agenturserver.de</Text>
+    <ContextualHelpTrigger>
+      <Button />
+      <ContextualHelp>
+        <Text>
+          Hier wird die URL des Servers angezeigt, der für
+          den E-Mail-Versand genutzt wird.
+        </Text>
+      </ContextualHelp>
+    </ContextualHelpTrigger>
+  </Align>
+</Column>;
