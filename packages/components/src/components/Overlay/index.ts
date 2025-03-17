375ed0ee3126adc00dced0282f0ea550f31de81b
lmeyer <l.meyer@mittwald.de>
2025-03-17 09:03:32 +0100

    fix(LightBox): close on click outside everywhere

--- packages/components/src/components/Overlay/index.ts
+++ packages/components/src/components/Overlay/index.ts
@@ -1,9 +1,7 @@
-import { Overlay } from "./Overlay";
-
 export { type OverlayProps, Overlay } from "./Overlay";
 export {
   OverlayContent,
   type OverlayContentProps,
 } from "./components/OverlayContent";
 
-export default Overlay;
+export { default } from "./Overlay";
