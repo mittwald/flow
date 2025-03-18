16cdfb4915be1097b5df4998257e77833b661473
Lisa Meyer <84317589+Lisa18289@users.noreply.github.com>
2025-03-18 09:13:08 +0100

    feat(AccentBox): add accent box component (#1279)

---
+++ apps/docs/src/content/03-components/structure/accent-box/examples/gradient.tsx
@@ -0,0 +1,23 @@
+import {
+  AccentBox,
+  Flex,
+  Heading,
+  Link,
+  Text,
+} from "@mittwald/flow-react-components";
+
+<AccentBox color="gradient">
+  <Flex align="center" wrap="wrap" gap="m">
+    <Flex direction="column" grow>
+      <Heading size="l">
+        mStudio Extension selber entwickeln
+      </Heading>
+      <Text color="dark">
+        <b>Veröffentliche eigene Features im mStudio</b>
+      </Text>
+    </Flex>
+    <Link target="_blank" href="#" color="dark">
+      Contributor Landingpage
+    </Link>
+  </Flex>
+</AccentBox>;
---
+++ packages/components/src/components/AccentBox/stories/Default.stories.tsx
@@ -0,0 +1,80 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { Heading } from "@/components/Heading";
+import { Text } from "@/components/Text";
+import { Link } from "@/components/Link";
+import { AccentBox } from "@/components/AccentBox";
+import { dummyText } from "@/lib/dev/dummyText";
+import { Section } from "@/components/Section";
+import { Flex } from "@/components/Flex";
+import { IconStar } from "@/components/Icon/components/icons";
+import { LayoutCard } from "@/components/LayoutCard";
+
+const meta: Meta<typeof AccentBox> = {
+  title: "Structure/AccentBox",
+  component: AccentBox,
+  args: { color: "blue" },
+  render: (props) => (
+    <AccentBox {...props}>
+      <Section>
+        <Heading>Heading</Heading>
+        <Text>{dummyText.medium}</Text>
+        <Link>Link</Link>
+      </Section>
+    </AccentBox>
+  ),
+};
+export default meta;
+
+type Story = StoryObj<typeof AccentBox>;
+
+export const Default: Story = {};
+
+export const Gradient: Story = {
+  args: { color: "gradient" },
+  render: (props) => (
+    <AccentBox {...props}>
+      <Flex align="center">
+        <Flex direction="column" grow>
+          <Heading size="l">Heading</Heading>
+          <Text color="dark">
+            <b>{dummyText.short}</b>
+          </Text>
+        </Flex>
+        <Link target="_blank" href="#" color="dark">
+          Link
+        </Link>
+      </Flex>
+    </AccentBox>
+  ),
+};
+
+export const WithIcon: Story = {
+  args: { color: "blue" },
+  render: (props) => (
+    <AccentBox {...props}>
+      <IconStar />
+      <Section>
+        <Heading>Heading</Heading>
+        <Text>{dummyText.long}</Text>
+        <Link>Link</Link>
+      </Section>
+    </AccentBox>
+  ),
+};
+
+export const InLayoutCard: Story = {
+  args: { color: "blue" },
+  render: (props) => (
+    <LayoutCard>
+      <AccentBox {...props}>
+        <IconStar />
+        <Section>
+          <Heading>Heading</Heading>
+          <Text>{dummyText.long}</Text>
+          <Link>Link</Link>
+        </Section>
+      </AccentBox>
+    </LayoutCard>
+  ),
+};
