3a05506b3119c3074763d84ea71307fc34dbf17f
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-03-20 10:46:48 +0100

    refact: refactor promise handling in RemoteRendererClient

--- packages/remote-react-renderer/src/RemoteRendererClient.tsx
+++ packages/remote-react-renderer/src/RemoteRendererClient.tsx
@@ -1,41 +1,25 @@
 "use client";
 
-import { components } from "@/components";
+import { useAwaiter } from "@/hooks/useAwaiter";
+import { useMergedComponents } from "@/hooks/useMergedComponents";
 import type { RemoteComponentsMap } from "@/lib/types";
-import type { RemoteComponentRendererProps } from "@mfalkenberg/remote-dom-react/host";
 import {
   RemoteReceiver,
   RemoteRootRenderer,
 } from "@mfalkenberg/remote-dom-react/host";
 import type { ExtBridgeRemoteApi } from "@mittwald/ext-bridge";
 import { connectRemoteIframeRef } from "@mittwald/flow-remote-core";
-import {
-  type ComponentType,
-  type CSSProperties,
-  type FC,
-  useLayoutEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import { reduce } from "remeda";
+import { usePromise } from "@mittwald/react-use-promise";
+import { type CSSProperties, type FC, useMemo, useState } from "react";
 
 export interface RemoteRendererProps {
   integrations?: RemoteComponentsMap<never>[];
   src: string;
-  timeout?: number;
+  timeoutMs?: number;
   extBridgeImplementation?: ExtBridgeRemoteApi;
 }
 
-interface PromiseObject {
-  result: null | Promise<void[]> | Error;
-  resolveRenderer: CallableFunction;
-  rejectRenderer: CallableFunction;
-  resolveConnection: CallableFunction;
-  rejectConnection: CallableFunction;
-}
-
-const HiddenIframeStyle: CSSProperties = {
+const hiddenIframeStyle: CSSProperties = {
   visibility: "hidden",
   height: 0,
   width: 0,
@@ -44,133 +28,75 @@ const HiddenIframeStyle: CSSProperties = {
   marginLeft: "-9999px",
 };
 
-const voidFunction = () => null;
-
 export const RemoteRendererClient: FC<RemoteRendererProps> = (props) => {
   const {
     integrations = [],
-    timeout = 10_000,
+    timeoutMs = 10_000,
     src,
     extBridgeImplementation,
   } = props;
 
-  const [, forceRerender] = useState<boolean>(false);
+  const [iframeRendered, setIframeRendered] = useState(false);
+  const renderAwaiter = useAwaiter([src]);
+  const connectionAwaiter = useAwaiter([src]);
+  const loadingAwaiter = useAwaiter([src]);
 
-  const iframeRef = useRef<HTMLIFrameElement>(null);
-  const checkRenderTimeout = useRef<number>(null);
-
-  const awaiter = useRef<PromiseObject>({
-    result: null,
-    resolveConnection: voidFunction,
-    rejectConnection: voidFunction,
-    resolveRenderer: voidFunction,
-    rejectRenderer: voidFunction,
-  }).current;
-
-  if (awaiter.result !== null) {
-    throw awaiter.result;
-  }
-
-  const clearRenderTimeout = () => {
-    if (checkRenderTimeout.current) {
-      clearTimeout(checkRenderTimeout.current);
-    }
-  };
-
-  const mergedComponents = useMemo(() => {
-    return new Map<string, ComponentType<RemoteComponentRendererProps>>(
-      Object.entries(
-        reduce(
-          [...integrations, components],
-          (merged, current) => ({
-            ...merged,
-            ...current,
-          }),
-          {},
-        ),
-      ),
-    );
-  }, [...integrations]);
+  const remoteComponents = useMergedComponents(integrations);
 
   const receiver = useMemo(() => {
     const remoteReceiver = new RemoteReceiver();
-    remoteReceiver.subscribe({ id: remoteReceiver.root.id }, () => {
-      awaiter.resolveRenderer();
-    });
-
+    remoteReceiver.subscribe(
+      { id: remoteReceiver.root.id },
+      renderAwaiter.resolve,
+    );
     return remoteReceiver;
-  }, []);
+  }, [src]);
 
   const connect = connectRemoteIframeRef({
     connection: receiver.connection,
     extBridgeImplementation: extBridgeImplementation,
-    onReady: () => {
-      awaiter.resolveConnection();
-    },
+    onReady: connectionAwaiter.resolve,
   });
 
-  useLayoutEffect(() => {
-    if (!src || !iframeRef.current || iframeRef.current.src === src) {
-      return;
-    }
+  const timeoutPromise = (message: string) =>
+    new Promise((_, rej) => {
+      setTimeout(() => {
+        rej(new Error(`${message}: Timeout reached`));
+      }, timeoutMs);
+    });
 
-    clearRenderTimeout();
-    iframeRef.current.src = src;
-
-    awaiter.result = Promise.all<void>([
-      new Promise((resolve, reject) => {
-        awaiter.resolveRenderer = () => {
-          clearRenderTimeout();
-          resolve();
-          awaiter.result = null;
-        };
-        awaiter.rejectRenderer = (reason: string) => {
-          clearRenderTimeout();
-          reject();
-          awaiter.result = new Error(reason);
-        };
-      }),
-      new Promise((resolve, reject) => {
-        awaiter.resolveConnection = () => {
-          resolve();
-          awaiter.result = null;
-        };
-        awaiter.rejectConnection = (reason: string) => {
-          reject();
-          awaiter.result = new Error(reason);
-        };
-      }),
+  const overallLoading = () =>
+    Promise.all([
+      Promise.race([
+        loadingAwaiter.promise,
+        timeoutPromise("Remote URL could not be loaded"),
+      ]),
+      Promise.race([
+        connectionAwaiter.promise,
+        timeoutPromise("Could not establish remote connection"),
+      ]),
+      Promise.race([
+        renderAwaiter.promise,
+        timeoutPromise("Remote rendering failed"),
+      ]),
     ]);
 
-    forceRerender((old) => !old);
-  }, [src, iframeRef]);
-
-  const onIframeLoaded = () => {
-    checkRenderTimeout.current = setTimeout(
-      () =>
-        awaiter.rejectRenderer(
-          "RemoteRenderTimeout reached. Remote URL was successfully loaded but no Remote Component was rendered in time.",
-        ),
-      timeout,
-    );
-  };
-
-  const onIframeError = () =>
-    awaiter.resolveRenderer(
-      "Remote URL could not be reached and was not loaded.",
-    );
+  usePromise(overallLoading, iframeRendered ? [] : null, {
+    loaderId: src,
+  });
 
   return (
     <>
-      <RemoteRootRenderer components={mergedComponents} receiver={receiver} />
+      <RemoteRootRenderer components={remoteComponents} receiver={receiver} />
       <iframe
+        src={src}
         ref={(ref) => {
-          iframeRef.current = ref;
-          return connect(ref);
+          connect(ref);
+          setIframeRendered(true);
         }}
-        onLoad={onIframeLoaded}
-        onError={onIframeError}
-        style={HiddenIframeStyle}
+        onLoad={loadingAwaiter.resolve}
+        onError={loadingAwaiter.reject}
+        style={hiddenIframeStyle}
       />
     </>
   );
