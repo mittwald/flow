d79d5aca5f3164608407621beeaf5c35f5ef013f
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-03-20 12:04:03 +0100

    fix: fix not refreshing when src changes

--- packages/remote-react-renderer/src/RemoteRendererClient.tsx
+++ packages/remote-react-renderer/src/RemoteRendererClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAwaiter } from "@/hooks/useAwaiter";
+import { useForceRerender } from "@/hooks/useForceRerender";
 import { useMergedComponents } from "@/hooks/useMergedComponents";
 import type { RemoteComponentsMap } from "@/lib/types";
 import {
@@ -10,7 +11,7 @@ import {
 import type { ExtBridgeRemoteApi } from "@mittwald/ext-bridge";
 import { connectRemoteIframeRef } from "@mittwald/flow-remote-core";
 import { usePromise } from "@mittwald/react-use-promise";
-import { type CSSProperties, type FC, useMemo, useState } from "react";
+import { type CSSProperties, type FC, useMemo, useRef } from "react";
 
 export interface RemoteRendererProps {
   integrations?: RemoteComponentsMap<never>[];
@@ -36,10 +37,12 @@ export const RemoteRendererClient: FC<RemoteRendererProps> = (props) => {
     extBridgeImplementation,
   } = props;
 
-  const [iframeRendered, setIframeRendered] = useState(false);
   const renderAwaiter = useAwaiter([src]);
   const connectionAwaiter = useAwaiter([src]);
   const loadingAwaiter = useAwaiter([src]);
+
+  const rendererSrc = useRef<string | null>(null);
+  const rerender = useForceRerender();
 
   const remoteComponents = useMergedComponents(integrations);
 
@@ -81,7 +84,8 @@ export const RemoteRendererClient: FC<RemoteRendererProps> = (props) => {
       ]),
     ]);
 
-  usePromise(overallLoading, iframeRendered ? [] : null, {
+  const awaitLoadingPromise = rendererSrc.current === src;
+  usePromise(overallLoading, awaitLoadingPromise ? [] : null, {
     loaderId: src,
   });
 
@@ -89,10 +93,12 @@ export const RemoteRendererClient: FC<RemoteRendererProps> = (props) => {
     <>
       <RemoteRootRenderer components={remoteComponents} receiver={receiver} />
       <iframe
+        key={src}
         src={src}
         ref={(ref) => {
           connect(ref);
-          setIframeRendered(true);
+          rendererSrc.current = src;
+          rerender();
         }}
         onLoad={loadingAwaiter.resolve}
         onError={loadingAwaiter.reject}
