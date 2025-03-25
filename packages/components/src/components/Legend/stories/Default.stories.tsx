546a9eba7203462d93b658d16003e073d3031214
Elissa Martinez <10544138+elmartinezc@users.noreply.github.com>
2025-03-25 13:53:42 +0100

    refactor: simplify legend story

--- packages/components/src/components/Legend/stories/Default.stories.tsx
+++ packages/components/src/components/Legend/stories/Default.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Legend from "../Legend";
 import { LegendItem } from "../components/LegendItem";
-import tokens from "@mittwald/flow-design-tokens/variables.json";
 
 const meta: Meta<typeof Legend> = {
   title: "Content/Legend",
@@ -9,30 +8,12 @@ const meta: Meta<typeof Legend> = {
   args: {},
   render: (props) => (
     <Legend {...props}>
-      <LegendItem
-        color={tokens.color.categorical["sea-green"].value}
-        title="Item 1"
-      />
-      <LegendItem
-        color={tokens.color.categorical["palatinate-blue"].value}
-        title="Item 2"
-      />
-      <LegendItem
-        color={tokens.color.categorical["tangerine"].value}
-        title="Item 3"
-      />
-      <LegendItem
-        color={tokens.color.categorical["magenta"].value}
-        title="Item 4"
-      />
-      <LegendItem
-        color={tokens.color.categorical["tropical-indigo"].value}
-        title="Item 5"
-      />
-      <LegendItem
-        color={tokens.color.categorical["malachite"].value}
-        title="Item 6"
-      />
+      <LegendItem color="sea-green" title="Item 1" />
+      <LegendItem color="palatinate-blue" title="Item 2" />
+      <LegendItem color="tangerine" title="Item 3" />
+      <LegendItem color="magenta" title="Item 4" />
+      <LegendItem color="tropical-indigo" title="Item 5" />
+      <LegendItem color="malachite" title="Item 6" />
     </Legend>
   ),
 };
