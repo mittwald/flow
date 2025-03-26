5642c290cc46f317c6de6ff3d299a046c3f2e6ae
Tim Auf dem Kampe <t.aufdemkampe@mittwald.de>
2025-03-26 13:04:16 +0100

    Add ContextMenu in FileCard example to docs

---
+++ apps/docs/src/content/03-components/upload/file-card/examples/contextMenu.tsx
@@ -0,0 +1,15 @@
+import {
+  ContextMenu,
+  FileCard,
+  MenuItem,
+} from "@mittwald/flow-react-components";
+
+<FileCard
+  type="image/jpg"
+  name="image.jpg"
+  sizeInBytes={47500}
+>
+  <ContextMenu>
+    <MenuItem>Entfernen</MenuItem>
+  </ContextMenu>
+</FileCard>;
