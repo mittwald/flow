ecd98471aa1d7cad90460e78d3f68ba677f54882
Marco Rieger <m.rieger@mittwald.de>
2025-03-27 16:31:07 +0100

    added slot root wrapper

--- packages/remote-react-components/src/auto-generated/CartesianGrid.ts
+++ packages/remote-react-components/src/auto-generated/CartesianGrid.ts
@@ -14,7 +14,7 @@ export const CartesianGrid = createFlowRemoteComponent(
   RemoteCartesianGridElement,
   {
     slotProps: {
-      wrapper: false,
+      wrapper: "flr-slot-root-wrapper",
     },
     eventProps: {},
   },
