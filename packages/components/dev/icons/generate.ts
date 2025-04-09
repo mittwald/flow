2b9f15bbe5cdadafaf35277589cdca6a28cfb0ae
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-04-09 11:00:35 +0200

    chore: fix NX caching issue with icon generation

--- packages/components/dev/icons/generate.ts
+++ packages/components/dev/icons/generate.ts
@@ -1,9 +1,10 @@
-import { prettify } from "./prettify";
-import { fs, writeIfHasChanges } from "./fs";
 import { getIconDefinitions } from "./definitions";
+import { fs } from "./fs";
+import { prettify } from "./prettify";
 import { getIconFileContent, getIndexFileContent } from "./template";
 
 const componentPath = "../../src/components/Icon";
+const iconsOutputFolder = fs.path(`${componentPath}/components/icons`);
 
 const getIconComponentFilename = (icon: string): string =>
   fs.path(`${componentPath}/components/icons/Icon${icon}.tsx`);
@@ -13,14 +14,15 @@ const getIndexFilename = (): string =>
 
 const definitions = getIconDefinitions(`${componentPath}/icons.yaml`);
 
+fs.find(iconsOutputFolder, {
+  matching: "*.tsx",
+}).forEach((file) => fs.remove(file));
+
 for (const [iconName, iconDefinition] of Object.entries(definitions)) {
-  writeIfHasChanges(
+  fs.write(
     getIconComponentFilename(iconName),
     await prettify(getIconFileContent(iconName, iconDefinition)),
   );
 }
 
-writeIfHasChanges(
-  getIndexFilename(),
-  await prettify(getIndexFileContent(definitions)),
-);
+fs.write(getIndexFilename(), await prettify(getIndexFileContent(definitions)));
