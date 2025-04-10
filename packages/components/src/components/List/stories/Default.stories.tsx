002ccd11e05a1a4156b2462a6bcd01f17acbb26c
lmeyer <l.meyer@mittwald.de>
2025-04-10 11:44:01 +0200

    feat(List): allow bottom position for summary

--- packages/components/src/components/List/stories/Default.stories.tsx
+++ packages/components/src/components/List/stories/Default.stories.tsx
@@ -180,6 +180,45 @@ export const WithSummary: Story = {
   },
 };
 
+export const WithSummaryBottom: Story = {
+  render: () => {
+    const InvoiceList = typedList<{
+      id: string;
+      date: string;
+      amount: string;
+    }>();
+
+    return (
+      <Section>
+        <Heading>Invoices</Heading>
+        <InvoiceList.List batchSize={5} aria-label="Invoices">
+          <ListSummary position="bottom">
+            <Text style={{ display: "block", textAlign: "right" }}>
+              <b>total: 42,00 €</b>
+            </Text>
+          </ListSummary>
+          <InvoiceList.StaticData
+            data={[
+              { id: "RG100000", date: "1.9.2024", amount: "25,00 €" },
+              { id: "RG100001", date: "12.9.2024", amount: "12,00 €" },
+              { id: "RG100002", date: "3.10.2024", amount: "4,00 €" },
+            ]}
+          />
+          <InvoiceList.Item>
+            {(invoice) => (
+              <ListItemView>
+                <Heading>{invoice.id}</Heading>
+                <Text>
+                  {invoice.date} - {invoice.amount}
+                </Text>
+              </ListItemView>
+            )}
+          </InvoiceList.Item>
+        </InvoiceList.List>
+      </Section>
+    );
+  },
+};
 export const WithAccordion: Story = {
   render: () => {
     const InvoiceList = typedList<{
