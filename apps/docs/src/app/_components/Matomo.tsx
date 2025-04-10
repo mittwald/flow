d634cd79247b24ad5f2dae2150ca163da5ae9d6e
lmeyer <l.meyer@mittwald.de>
2025-04-10 13:06:38 +0200

    docs: update matomo script

--- apps/docs/src/app/_components/Matomo.tsx
+++ apps/docs/src/app/_components/Matomo.tsx
@@ -1,30 +1,11 @@
 "use client";
 import { type FC, useEffect } from "react";
-import { isArray } from "remeda";
 
 export const Matomo: FC = () => {
   useEffect(() => {
-    const _paq = (
-      "_paq" in window && isArray(window._paq) ? window._paq : []
-    ) as unknown[];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(["trackPageView"]);
-    _paq.push(["enableLinkTracking"]);
-    (function () {
-      const u = "//td.mittwald.de/";
-      _paq.push(["setTrackerUrl", u + "matomo.php"]);
-      _paq.push(["setSiteId", "2"]);
-      const d = document,
-        g = d.createElement("script"),
-        s = d.getElementsByTagName("script")[0];
-      g.async = true;
-      g.src = u + "matomo.js";
-      s?.parentNode?.insertBefore(g, s);
-    })();
-
-    const _mtm = (
-      "_mtm" in window && isArray(window._mtm) ? window._mtm : []
-    ) as unknown[];
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const _mtm = (window._mtm = window._mtm || []);
     _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
     (function () {
       const d = document,
