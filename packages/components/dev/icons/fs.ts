2f1f09343942c5abb2454ae93bb0eaab13cf2629
Marco Falkenberg <mfal@users.noreply.github.com>
2025-04-10 11:47:42 +0200

    chore: fix NX caching issue with icon generation (#1354)

--- packages/components/dev/icons/fs.ts
+++ packages/components/dev/icons/fs.ts
@@ -1,10 +1,3 @@
 import jetpack from "fs-jetpack";
 
 export const fs = jetpack.cwd(import.meta.dirname);
-
-export const writeIfHasChanges = (path: string, content: string): void => {
-  const currentContent = fs.read(path);
-  if (currentContent !== content) {
-    fs.write(path, content);
-  }
-};
