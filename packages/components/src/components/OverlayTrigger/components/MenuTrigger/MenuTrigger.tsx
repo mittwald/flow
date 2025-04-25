26aa76e93485296640c804891bd17500b15d1d57
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-04-25 10:52:29 +0200

    fix(Remote): fix context menu position when used in section header

--- packages/components/src/components/OverlayTrigger/components/MenuTrigger/MenuTrigger.tsx
+++ packages/components/src/components/OverlayTrigger/components/MenuTrigger/MenuTrigger.tsx
@@ -1,11 +1,16 @@
 import * as Aria from "react-aria-components";
 import type { ComponentProps, FC } from "react";
+import { flowComponent, type FlowComponentProps } from "@/internal";
 
-export type MenuTriggerProps = ComponentProps<typeof Aria.MenuTrigger>;
+export type MenuTriggerProps = ComponentProps<typeof Aria.MenuTrigger> &
+  FlowComponentProps;
 
 /** @flr-generate all */
-export const MenuTrigger: FC<MenuTriggerProps> = (props) => {
-  return <Aria.MenuTrigger {...props} />;
-};
+export const MenuTrigger: FC<MenuTriggerProps> = flowComponent(
+  "MenuTrigger",
+  (props) => {
+    return <Aria.MenuTrigger {...props} />;
+  },
+);
 
 export default MenuTrigger;
