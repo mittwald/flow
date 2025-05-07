bd753505900af6fc839c13e1416121f05dd30c20
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-05-07 09:28:14 +0200

    add test

--- packages/react-tunnel/src/Tunnel.test.tsx
+++ packages/react-tunnel/src/Tunnel.test.tsx
@@ -155,3 +155,37 @@ test("Fallback content in exit is rendered again when not rendering entry", asyn
 
   expect(dom.getByTestId("exit").innerText).toBe("Fallback!");
 });
+
+test("Render function in TunnelExit gets children from TunnelEntry", async () => {
+  const dom = render(
+    <TunnelProvider>
+      <div data-testid="exit">
+        <TunnelExit>{(children) => <>{children} Tunnel!</>}</TunnelExit>
+      </div>
+      <TunnelEntry>Hello</TunnelEntry>
+    </TunnelProvider>,
+  );
+
+  expect(dom.getByTestId("exit").innerText).toBe("Hello Tunnel!");
+});
+
+test("Render function in TunnelExit gets updated children from TunnelEntry", async () => {
+  const dom = render(
+    <TunnelProvider>
+      <div data-testid="exit">
+        <TunnelExit>{(children) => <>{children} Tunnel!</>}</TunnelExit>
+      </div>
+      <TunnelEntry>Hello</TunnelEntry>
+    </TunnelProvider>,
+  );
+
+  dom.rerender(
+    <TunnelProvider>
+      <div data-testid="exit">
+        <TunnelExit>{(children) => <>{children} Tunnel!</>}</TunnelExit>
+      </div>
+      <TunnelEntry>Hi</TunnelEntry>
+    </TunnelProvider>,
+  );
+  expect(dom.getByTestId("exit").innerText).toBe("Hi Tunnel!");
+});
