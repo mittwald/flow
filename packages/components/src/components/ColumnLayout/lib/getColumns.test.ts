54e4d76eadbc36b845e16bfc7210b81edf85ba92
lmeyer <l.meyer@mittwald.de>
2025-05-15 15:11:20 +0200

    feat(List): support column layout in list item

--- packages/components/src/components/ColumnLayout/lib/getColumns.test.ts
+++ packages/components/src/components/ColumnLayout/lib/getColumns.test.ts
@@ -2,10 +2,6 @@ import { describe, expect, test } from "vitest";
 import { getColumns } from "./getColumns";
 
 describe('"getColumns()', () => {
-  test("does throw error if array is empty", () => {
-    expect(() => getColumns([])).toThrowError();
-  });
-
   test.each([
     [[1], "1fr"],
     [[1, 2, 3], "1fr 2fr 3fr"],
