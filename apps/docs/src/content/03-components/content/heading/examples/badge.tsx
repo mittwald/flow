736672fdd01a8fd0da889cde77d1da802183081e
lmeyer <l.meyer@mittwald.de>
2025-05-19 14:09:51 +0200

    docs: add badge example

---
+++ apps/docs/src/content/03-components/content/heading/examples/badge.tsx
@@ -0,0 +1,11 @@
+import {
+  AlertBadge,
+  Heading,
+} from "@mittwald/flow-react-components";
+
+<Heading>
+  E-Mail-Adresse
+  <AlertBadge status="danger">
+    Speicherplatz voll
+  </AlertBadge>
+</Heading>;
