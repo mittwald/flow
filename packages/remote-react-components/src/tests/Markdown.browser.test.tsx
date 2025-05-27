8c3511451311b466ec7e97ca23f3fb0781c1b155
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-05-27 16:38:28 +0200

    test(Markdown): add basic test

---
+++ packages/remote-react-components/src/tests/Markdown.browser.test.tsx
@@ -0,0 +1,11 @@
+import { renderRemoteTest } from "@/tests/renderRemoteTest";
+import { expect, test } from "vitest";
+
+test("Markdown is rendered", async () => {
+  const dom = renderRemoteTest("standard");
+  const markdown = dom.getByTestId("markdown");
+  await expect.element(markdown).toBeInTheDocument();
+  expect(markdown.element().innerHTML).toMatchInlineSnapshot(
+    `"<p class="flow--text flow--text--align-start">This is <strong>important</strong></p>"`,
+  );
+});
