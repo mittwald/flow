a06ee6dd7727a0e9b290b74f8b0b83b1b5b2c6db
Marco Rieger <m.rieger@mittwald.de>
2025-06-03 14:41:19 +0200

    temporary removed worker script

--- packages/components/src/components/PasswordCreationField/worker/generatePassword.ts
+++ packages/components/src/components/PasswordCreationField/worker/generatePassword.ts
@@ -4,31 +4,5 @@ import { Generator } from "@mittwald/password-tools-js/generator";
 export const generatePassword = async (
   validationPolicy: Policy,
 ): Promise<string> => {
-  if (typeof Worker === "undefined") {
-    return new Generator(validationPolicy).generatePassword();
-  }
-
-  const { default: GeneratorWorker } = await import(
-    `./passwordGeneration.worker?worker&inline`
-  );
-
-  const worker = new GeneratorWorker();
-  return new Promise((resolve, reject) => {
-    worker.onmessage = (e: MessageEvent) => {
-      const [action, data] = e.data;
-      if (action === "generatePassword") {
-        resolve(data);
-      } else if (action === "error") {
-        reject(data);
-      }
-    };
-
-    worker.postMessage([
-      "generatePassword",
-      JSON.stringify({
-        ...validationPolicy,
-        rules: [...validationPolicy.rules].map((r) => r.config),
-      }),
-    ]);
-  });
+  return new Generator(validationPolicy).generatePassword();
 };
