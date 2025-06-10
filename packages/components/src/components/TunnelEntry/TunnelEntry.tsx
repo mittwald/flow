a9982f0771d3b5ec9bba1ce3e9f49ac855e19e9b
Marco Rieger <m.rieger@mittwald.de>
2025-06-10 15:00:33 +0200

    use correct type

--- packages/components/src/components/TunnelEntry/TunnelEntry.tsx
+++ packages/components/src/components/TunnelEntry/TunnelEntry.tsx
@@ -1,10 +1,12 @@
 import { TunnelEntry as ReactTunnelEntry } from "@mittwald/react-tunnel";
 
+export type TunnelEntryProps = typeof ReactTunnelEntry;
+
 /**
  * @flr-generate all
  * @flr-clear-props-context
  */
-export const TunnelEntry: typeof ReactTunnelEntry = (props) => (
+export const TunnelEntry: TunnelEntryProps = (props) => (
   <ReactTunnelEntry {...props} />
 );
 
