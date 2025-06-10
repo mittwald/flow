a88ee2a08ba90a4595e4c025dba16bb90293eb0c
Marco Rieger <m.rieger@mittwald.de>
2025-06-10 17:27:57 +0200

    build new formdata

--- packages/remote-react-renderer/src/components/lib/prepareFormData.ts
+++ packages/remote-react-renderer/src/components/lib/prepareFormData.ts
@@ -3,17 +3,23 @@ import { addAwaitedArrayBuffer } from "@mittwald/flow-core";
 export const prepareFormData = async (
   formData: FormData,
 ): Promise<FormData> => {
-  for (const [fieldName, value] of formData.entries()) {
-    if (value instanceof File) {
-      await addAwaitedArrayBuffer(value);
-
-      if (formData.has(fieldName)) {
-        formData.append(fieldName, value);
-      } else {
-        formData.set(fieldName, value);
+  const preparedFormData = new FormData();
+  for (const [fieldName, value] of formData) {
+    const preparedValue = value;
+    if (preparedValue instanceof File) {
+      if (!preparedValue.name || preparedValue.size <= 0) {
+        break;
       }
+
+      await addAwaitedArrayBuffer(preparedValue);
+    }
+
+    if (preparedFormData.has(fieldName)) {
+      preparedFormData.append(fieldName, preparedValue);
+    } else {
+      preparedFormData.set(fieldName, preparedValue);
     }
   }
 
-  return formData;
+  return preparedFormData;
 };
