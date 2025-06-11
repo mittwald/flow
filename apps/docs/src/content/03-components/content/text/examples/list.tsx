7b718f0b9e494bef656d30d70653bfb6d6a5af01
lmeyer <l.meyer@mittwald.de>
2025-06-11 14:14:35 +0200

    docs: add list example to text documentation

--- apps/docs/src/content/03-components/content/text/examples/list.tsx
+++ apps/docs/src/content/03-components/content/text/examples/list.tsx
@@ -1,12 +1,16 @@
 import { Text } from "@mittwald/flow-react-components";
 
-<Text>
-  <ul>
-    <li>Item 1</li>
-    <li>Item 2</li>
-  </ul>
-  <ol>
-    <li>Item 1</li>
-    <li>Item 2</li>
-  </ol>
-</Text>;
+<Column>
+  <Text>
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ul>
+  </Text>
+  <Text>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+    </ol>
+  </Text>
+</Column>;
