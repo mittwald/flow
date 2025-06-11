ed77835410935258124bf8de8341ccf1ef2a26b4
Marco Rieger <m.rieger@mittwald.de>
2025-06-11 16:46:38 +0200

    add basepath

--- packages/components/src/integrations/nextjs/components/RouterProvider/RouterProvider.tsx
+++ packages/components/src/integrations/nextjs/components/RouterProvider/RouterProvider.tsx
@@ -1,11 +1,12 @@
-import type { FC, PropsWithChildren } from "react";
+import { type FC, type PropsWithChildren } from "react";
 import { RouterProvider as BaseRouterProvider } from "react-aria-components";
 import { useRouter } from "next/navigation";
+import { addBasePath } from "next/dist/client/add-base-path";
 
 export const RouterProvider: FC<PropsWithChildren> = (props) => {
   const router = useRouter();
   return (
-    <BaseRouterProvider navigate={router.push}>
+    <BaseRouterProvider navigate={router.push} useHref={addBasePath}>
       {props.children}
     </BaseRouterProvider>
   );
