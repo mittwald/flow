a5f9aa03c28c3bac04978e8062aa8c71ec8d5d86
lmeyer <l.meyer@mittwald.de>
2025-06-12 13:29:48 +0200

    docs: add donut chart docs

--- apps/docs/src/content/03-components/data-visualisation/donut-chart/examples/segments.tsx
+++ apps/docs/src/content/03-components/data-visualisation/donut-chart/examples/segments.tsx
@@ -1,11 +1,13 @@
 import { DonutChart } from "@mittwald/flow-react-components";
 
 <DonutChart
-  showLegend="bottom"
+  showLegend="right"
   segments={[
     { title: "Item 1", value: 28 },
     { title: "Item 2", value: 24 },
     { title: "Item 3", value: 20 },
     { title: "Item 4", value: 10 },
+    { title: "Item 5", value: 12 },
+    { title: "Item 6", value: 6 },
   ]}
 />;
