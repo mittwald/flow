aae1fe94275a149752a8b47920d679586041d084
lmeyer <l.meyer@mittwald.de>
2025-06-17 14:06:02 +0200

    tests: add remote component tests

--- packages/remote-react-components/src/tests/Alert.browser.test.tsx
+++ packages/remote-react-components/src/tests/Alert.browser.test.tsx
@@ -5,8 +5,50 @@ test("Alert is rendered", async () => {
   const dom = renderRemoteTest("standard");
   const alert = dom.getByTestId("alert");
   await expect.element(alert).toBeInTheDocument();
-  const alertHeading = dom.getByTestId("alertHeading");
-  await expect.element(alertHeading).toBeInTheDocument();
-  expect(alert.element()).toHaveTextContent("Email address has been archived");
-  expect(alertHeading.element()).toHaveClass("flow--alert--heading");
+  expect(alert.element()).toMatchInlineSnapshot(`
+    <aside
+      class="flow--alert flow--alert--info"
+      data-testid="alert"
+    >
+      <h3
+        class="flow--heading flow--heading--s flow--alert--heading"
+        data-testid="alertHeading"
+      >
+        <span
+          class="flow--heading--heading-text"
+        >
+          <svg
+            aria-hidden="false"
+            aria-label="Status Information"
+            class="tabler-icon tabler-icon-info-circle flow--icon flow--alert--icon flow--icon--size-m flow--icon--info"
+            fill="none"
+            focusable="false"
+            height="24"
+            role="img"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"
+            />
+            <path
+              d="M12 9h.01"
+            />
+            <path
+              d="M11 12h1v4h1"
+            />
+          </svg>
+          Email address has been archived
+        </span>
+        <span
+          class="flow--heading--heading-content"
+        />
+      </h3>
+    </aside>
+  `);
 });
--- packages/remote-react-components/src/tests/AlertBadge.browser.test.tsx
+++ packages/remote-react-components/src/tests/AlertBadge.browser.test.tsx
@@ -5,6 +5,42 @@ test("AlertBadge is rendered", async () => {
   const dom = renderRemoteTest("standard");
   const alertBadge = dom.getByTestId("alertBadge");
   await expect.element(alertBadge).toBeInTheDocument();
-  expect(alertBadge.element()).toHaveClass("flow--alert-badge--info");
-  expect(alertBadge.element()).toHaveTextContent("Info");
+  expect(alertBadge.element()).toMatchInlineSnapshot(`
+    <div
+      class="flow--alert-badge flow--alert-badge--info"
+      data-testid="alertBadge"
+    >
+      <svg
+        aria-hidden="false"
+        aria-label="Status Information"
+        class="tabler-icon tabler-icon-info-circle flow--icon flow--icon--size-s flow--icon--info"
+        fill="none"
+        focusable="false"
+        height="24"
+        role="img"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"
+        />
+        <path
+          d="M12 9h.01"
+        />
+        <path
+          d="M11 12h1v4h1"
+        />
+      </svg>
+      <span
+        class="flow--text flow--text--align-start flow--alert-badge--text"
+      >
+        Info
+      </span>
+    </div>
+  `);
 });
