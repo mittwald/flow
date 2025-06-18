5e02262c2a8975cf6f5cd786a5d1e9218db564ca
lmeyer <l.meyer@mittwald.de>
2025-06-18 07:41:42 +0200

    docs: fix accent box docs

--- apps/docs/src/content/03-components/structure/accent-box/examples/color.tsx
+++ apps/docs/src/content/03-components/structure/accent-box/examples/color.tsx
@@ -6,8 +6,8 @@ import {
   Heading,
   Icon,
   Link,
-  Section,
   Rating,
+  Section,
   Text,
 } from "@mittwald/flow-react-components";
 import {
@@ -25,13 +25,16 @@ import {
         Hilf uns, Flow noch besser zu machen!
       </Heading>
       <Text>
-        Fehlt dir eine bestimmte Component oder etwas anderes? Hast du Feedback? Dann teile es uns gerne auf GitHub mit.
+        Fehlt dir eine bestimmte Component oder etwas
+        anderes? Hast du Feedback? Dann teile es uns gerne
+        auf GitHub mit.
       </Text>
       <Link href="#" target="_blank">
         Feedback zu Flow geben
       </Link>
     </Section>
   </AccentBox>
+
   <AccentBox color="green">
     <Icon>
       <IconLeaf />
@@ -40,10 +43,10 @@ import {
       <Heading>Tipps & Tricks für mehr Klimaschutz</Heading>
       <Text>
         Dein Cronjob läuft in weniger als
-        5-Minuten-Intervallen. Das verbraucht mehr Ressourcen.
-        Falls das nicht unbedingt nötig ist, kannst du ein
-        längeres Intervall wählen - das spart Energie und
-        schützt das Klima
+        5-Minuten-Intervallen. Das verbraucht mehr
+        Ressourcen. Falls das nicht unbedingt nötig ist,
+        kannst du ein längeres Intervall wählen - das spart
+        Energie und schützt das Klima
       </Text>
       <Link href="#" target="_blank">
         Blogartikel zu mehr Nachhaltigkeit
@@ -52,30 +55,30 @@ import {
   </AccentBox>
 
   <ColumnLayout>
-  <AccentBox color="neutral">
-    <Flex direction="column" gap="s" align="center">
-      <BigNumber>
-        <Text>250 ms</Text>
-        <Text>Dateioperationen</Text>
-      </BigNumber>
-      <Rating value={4} />
-      <Text>
-        <small>Geringer Optimierungsbedarf</small>
-      </Text>
-    </Flex>
-  </AccentBox>
-  <AccentBox color="neutral">
-    <Flex direction="column" gap="xs" align="center">
-      <BigNumber>
-        <Text>100 ms</Text>
-        <Text>Serveroperationen</Text>
-      </BigNumber>
-      <Rating value={2} />
-      <Text>
-        <small>Optimierungsbedarf</small>
-      </Text>
-    </Flex>
-  </AccentBox>
-</ColumnLayout>
-  
+    <AccentBox color="neutral">
+      <Flex direction="column" gap="s" align="center">
+        <BigNumber>
+          <Text>250 ms</Text>
+          <Text>Dateioperationen</Text>
+        </BigNumber>
+        <Rating value={4} />
+        <Text>
+          <small>Geringer Optimierungsbedarf</small>
+        </Text>
+      </Flex>
+    </AccentBox>
+
+    <AccentBox color="neutral">
+      <Flex direction="column" gap="xs" align="center">
+        <BigNumber>
+          <Text>100 ms</Text>
+          <Text>Serveroperationen</Text>
+        </BigNumber>
+        <Rating value={2} />
+        <Text>
+          <small>Optimierungsbedarf</small>
+        </Text>
+      </Flex>
+    </AccentBox>
+  </ColumnLayout>
 </Section>;
