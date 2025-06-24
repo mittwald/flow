09b65c507c5fbfdcd2f77c634a920f3f71c0f58b
nmey <n.meyer@mittwald.de>
2025-06-24 14:29:50 +0200

    docs: Update CodeBlock LiveCodeEditor Example copyable.tsx

--- apps/docs/src/content/03-components/content/code-block/examples/copyable.tsx
+++ apps/docs/src/content/03-components/content/code-block/examples/copyable.tsx
@@ -2,6 +2,7 @@ import { CodeBlock } from "@mittwald/flow-react-components";
 
 <CodeBlock
   copyable
+  showLineNumbers
   language="json"
   code={`{
     "projectId": "b3a96db5-ba8f-40dd-9100-bab43ac1f698",
