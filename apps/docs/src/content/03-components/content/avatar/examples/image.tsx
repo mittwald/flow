19355358e3d272c32ae16e0fb5c91211cc7815bc
kati-ux <ka.hoffmann@mittwald.de>
2025-06-25 09:12:32 +0200

    docs: Update Avatar Example image.tsx

--- apps/docs/src/content/03-components/content/avatar/examples/image.tsx
+++ apps/docs/src/content/03-components/content/avatar/examples/image.tsx
@@ -1,11 +1,8 @@
 import {
   Avatar,
-  Image,
+  Initials,
 } from "@mittwald/flow-react-components";
 
 <Avatar>
-  <Image
-    alt="Gopher"
-    src="https://cdn.shopify.com/s/files/1/2022/6883/products/IMG_2002_250x250@2x.JPG?v=1538235544"
-  />
+  <Initials>Gillian Gopher</Initials>
 </Avatar>;
