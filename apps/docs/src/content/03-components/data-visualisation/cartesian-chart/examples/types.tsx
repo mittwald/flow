7b19f934d458e7755188b1c744a5d1e68909cd5d
lstockmann <l.stockmann@mittwald.de>
2025-07-31 12:51:39 +0200

    docs: update chart docs

--- apps/docs/src/content/03-components/data-visualisation/cartesian-chart/examples/types.tsx
+++ apps/docs/src/content/03-components/data-visualisation/cartesian-chart/examples/types.tsx
@@ -3,42 +3,56 @@ import {
   Line,
   CartesianChart,
   CartesianGrid,
-  ChartTooltip,
-  ChartLegend,
   YAxis,
   XAxis,
 } from "@mittwald/flow-react-components";
 
-<CartesianChart
-  data={[
+export default () => {
+  const data = [
     {
-      Zeit: "0 Uhr",
-      Durchschnitt: 40,
-      Maximum: 45,
+      Zeit: "A",
+      A: 40,
+      B: 45,
     },
     {
-      Zeit: "1 Uhr",
-      Durchschnitt: 32,
-      Maximum: 35,
+      Zeit: "B",
+      A: 32,
+      B: 35,
     },
     {
-      Zeit: "2 Uhr",
-      Durchschnitt: 20,
-      Maximum: 78,
+      Zeit: "C",
+      A: 20,
+      B: 78,
     },
     {
-      Zeit: "3 Uhr",
-      Durchschnitt: 27,
-      Maximum: 39,
+      Zeit: "D",
+      A: 27,
+      B: 39,
     },
-  ]}
-  height="300px"
->
-  <CartesianGrid />
-  <Area dataKey="Durchschnitt" />
-  <Line dataKey="Maximum" color="magenta" />
-  <XAxis dataKey="Zeit" />
-  <YAxis domain={[0, 100]} unit=" %" />
-  <ChartTooltip formatter={(v, k) => `${k}: ${v} %`} />
-  <ChartLegend />
-</CartesianChart>;
+  ];
+  return (
+    <Row>
+      <CartesianChart flexGrow data={data} height="300px">
+        <CartesianGrid />
+        <Area dataKey="A" />
+        <Area dataKey="B" color="palatinate-blue" />
+        <XAxis dataKey="Zeit" />
+        <YAxis domain={[0, 100]} unit=" %" />
+      </CartesianChart>
+      <CartesianChart flexGrow data={data} height="300px">
+        <CartesianGrid />
+        <Line dataKey="A" />
+        <Line dataKey="B" color="palatinate-blue" />
+        <XAxis dataKey="Zeit" />
+        <YAxis domain={[0, 100]} unit=" %" />
+      </CartesianChart>
+      <CartesianChart flexGrow data={data} height="300px">
+        <CartesianGrid />
+        <Area dataKey="A" />
+        <Line dataKey="B" color="palatinate-blue" />
+        <XAxis dataKey="Zeit" />
+        <YAxis domain={[0, 100]} unit=" %" />
+      </CartesianChart>
+    </Row>
+  );
+};
