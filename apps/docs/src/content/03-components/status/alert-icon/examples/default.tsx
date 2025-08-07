a1824279dc23c5ac009a9788e7d5f134dc6d3cee
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-08-07 12:57:40 +0200

    docs: remove alert icon from docs (#1829)

--- apps/docs/src/content/03-components/status/alert-icon/examples/default.tsx
+++
@@ -1,3 +0,0 @@
-import { AlertIcon } from "@mittwald/flow-react-components";
-
-<AlertIcon />;
--- apps/docs/src/content/03-components/status/alert-icon/examples/status.tsx
+++
@@ -1,8 +0,0 @@
-import { AlertIcon } from "@mittwald/flow-react-components";
-
-<Row>
-  <AlertIcon status="info" />
-  <AlertIcon status="success" />
-  <AlertIcon status="warning" />
-  <AlertIcon status="danger" />
-</Row>;
