2389ad0d2d719dd6b0f0ab01f1a062e90378b764
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-08-13 08:39:21 +0200

    fix(Modal): fix missing slot attribute (#1873)

--- packages/components/src/components/Modal/Modal.tsx
+++ packages/components/src/components/Modal/Modal.tsx
@@ -1,10 +1,12 @@
-import type { PropsWithChildren } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
 import React from "react";
 import styles from "./Modal.module.scss";
 import clsx from "clsx";
-import type { PropsContext } from "@/lib/propsContext";
-import { PropsContextProvider } from "@/lib/propsContext";
-import { TunnelExit, TunnelProvider } from "@mittwald/react-tunnel";
+import {
+  dynamic,
+  type PropsContext,
+  PropsContextProvider,
+} from "@/lib/propsContext";
 import type { OverlayController } from "@/lib/controller/overlay";
 import type { FlowComponentProps } from "@/lib/componentFactory/flowComponent";
 import { flowComponent } from "@/lib/componentFactory/flowComponent";
@@ -54,8 +56,23 @@ export const Modal = flowComponent("Modal", (props) => {
   const rootClassName = clsx(
     offCanvas ? styles.offCanvas : styles.modal,
     styles[`size-${size}`],
-    styles[offCanvasOrientation],
+    offCanvasOrientation === "left" && styles["left"],
     className,
+  );
+
+  const header = (children: ReactNode) => (
+    <HeaderView className={styles.header}>
+      {children}
+      <Action closeOverlay="Modal">
+        <ButtonView
+          variant="plain"
+          color="secondary"
+          onPress={controller?.close}
+        >
+          <IconClose />
+        </ButtonView>
+      </Action>
+    </HeaderView>
   );
 
   const propsContext: PropsContext = {
@@ -86,7 +103,7 @@ export const Modal = flowComponent("Modal", (props) => {
     Heading: {
       level: 2,
       slot: "title",
-      tunnelId: "heading",
+      children: dynamic((props) => header(props.children)),
     },
     ActionGroup: {
       className: styles.actionGroup,
@@ -102,22 +119,7 @@ export const Modal = flowComponent("Modal", (props) => {
       {...rest}
     >
       <PropsContextProvider props={propsContext}>
-        <TunnelProvider>
-          <HeaderView className={styles.header}>
-            <TunnelExit id="heading" />
-            <Action closeOverlay="Modal">
-              <ButtonView
-                variant="plain"
-                color="secondary"
-                className={styles.closeButton}
-                onPress={controller?.close}
-              >
-                <IconClose />
-              </ButtonView>
-            </Action>
-          </HeaderView>
-          {children}
-        </TunnelProvider>
+        {children}
       </PropsContextProvider>
     </Overlay>
   );
