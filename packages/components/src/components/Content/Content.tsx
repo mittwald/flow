196a07304e790171f76459e09233354dbe2d7f2d
lstockmann <l.stockmann@mittwald.de>
2025-09-11 10:16:44 +0200

    fix(Modal): clear content props context

--- packages/components/src/components/Content/Content.tsx
+++ packages/components/src/components/Content/Content.tsx
@@ -1,13 +1,17 @@
 import type { PropsWithChildren } from "react";
 import React from "react";
+import ClearPropsContext from "@/components/ClearPropsContext/ClearPropsContext";
 import type { PropsWithElementType } from "@/lib/types/props";
 import type { FlowComponentProps } from "@/lib/componentFactory/flowComponent";
 import { flowComponent } from "@/lib/componentFactory/flowComponent";
+import { Wrap } from "@/components/Wrap";
 
 export interface ContentProps
   extends PropsWithChildren,
     PropsWithElementType<"div" | "section" | "span">,
     FlowComponentProps {
+  /** @internal */
+  clearPropsContext?: boolean;
   /** @internal */
   slot?: string;
 }
@@ -17,14 +21,24 @@ export interface ContentProps
  * @flr-clear-props-context
  */
 export const Content = flowComponent("Content", (props) => {
-  const { children, elementType = "div", ref, ...rest } = props;
+  const {
+    children,
+    elementType = "div",
+    ref,
+    clearPropsContext = false,
+    ...rest
+  } = props;
 
   const Element = elementType;
 
   return (
-    <Element ref={ref} {...rest}>
-      {children}
-    </Element>
+    <Wrap if={clearPropsContext}>
+      <ClearPropsContext>
+        <Element ref={ref} {...rest}>
+          {children}
+        </Element>
+      </ClearPropsContext>
+    </Wrap>
   );
 });
 
