c97346edb1ebc382bd6b355ac4bd0a7c808523c7
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-09-15 15:05:46 +0200

    fix(Field): show message for max and min length (#1982)

--- packages/components/src/integrations/react-hook-form/components/Field/stories/TextField.stories.tsx
+++ packages/components/src/integrations/react-hook-form/components/Field/stories/TextField.stories.tsx
@@ -20,6 +20,7 @@ const meta: Meta<typeof Field> = {
       nameDefaultValue: string;
       nameRequired: string;
       nameMaxLength: string;
+      nameMinLength: string;
       controlledName: string;
     }
 
@@ -74,6 +75,18 @@ const meta: Meta<typeof Field> = {
 
           <Field name="controlledName">
             <TextField value={form.watch("controlledName").toUpperCase()}>
+              <Label>Name</Label>
+            </TextField>
+          </Field>
+
+          <Field
+            name="nameMinLength"
+            rules={{
+              required: "Please enter your name",
+              minLength: 2,
+            }}
+          >
+            <TextField>
               <Label>Name</Label>
             </TextField>
           </Field>
