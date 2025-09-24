f3da8676b07d18faefdf882d22c9cc9ed739ef53
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-09-24 10:46:12 +0200

    docs: refactor layout (#1996)
    * docs: refactor layout
    
    * docs: refactor layout
    
    * docs: refactor layout
    
    * docs: hide tab panel overflow
    
    * docs: add margin to navigation

--- apps/docs/src/app/_components/layout/MainContent/MainContent.tsx
+++ apps/docs/src/app/_components/layout/MainContent/MainContent.tsx
@@ -1,5 +1,4 @@
-import type { FC } from "react";
-import React from "react";
+import React, { type FC } from "react";
 import type { MdxFile } from "@/lib/mdx/MdxFile";
 import MdxFileView from "@/lib/mdx/components/MdxFileView";
 import { Tab, TabTitle } from "@mittwald/flow-react-components";
