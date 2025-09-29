de924025d18fa979e37b2279808d03bdf31e9dcf
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-09-29 09:41:54 +0200

    WIP

--- packages/remote-react-components/dev/vitest/remote-test-server/setupGlobal.ts
+++ packages/remote-react-components/dev/vitest/remote-test-server/setupGlobal.ts
@@ -3,8 +3,7 @@ import { createTestServer } from "./createTestServer";
 const testServer = await createTestServer();
 
 export async function setup() {
-  void testServer.start();
-  await new Promise((res) => setTimeout(res, 2000));
+  await testServer.start();
 }
 
 export async function teardown() {
