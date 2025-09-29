10ab33b45bc986a56598f98a370bda8ed4c2856b
Marco Falkenberg <m.falkenberg@mittwald.de>
2025-09-29 11:24:35 +0200

    Revert "WIP"
    This reverts commit 08a1ff7f2800c034207113a5aceccb671f6545b8.

--- packages/remote-react-components/src/tests/Button.browser.test.remote.tsx
+++ packages/remote-react-components/src/tests/Button.browser.test.remote.tsx
@@ -1,12 +1,23 @@
 import { Button, Text } from "@/auto-generated";
+import { useState } from "react";
 
 export const standard = () => <Button data-testid="button">Click me</Button>;
 
 export const eventhandler = () => {
+  const [eventData, setEventData] = useState<object>({});
   return (
     <>
-      <Text data-testid="event-data">test</Text>
-      <Button data-testid="button">Click me</Button>
+      <Text data-testid="event-data">
+        {JSON.stringify({
+          ...eventData,
+          // Mute flaky position
+          x: undefined,
+          y: undefined,
+        })}
+      </Text>
+      <Button data-testid="button" onPress={setEventData}>
+        Click me
+      </Button>
     </>
   );
 };
