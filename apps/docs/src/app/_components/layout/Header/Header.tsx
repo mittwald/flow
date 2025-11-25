b96f6521018196a258fe2e1bfb8dfd9d10bd698b
lstockmann <l.stockmann@mittwald.de>
2025-11-25 07:11:11 +0100

    docs: update page layout

--- apps/docs/src/app/_components/layout/Header/Header.tsx
+++ apps/docs/src/app/_components/layout/Header/Header.tsx
@@ -1,20 +1,37 @@
-import type { FC } from "react";
+"use client";
+import { type FC, useEffect, useState } from "react";
 import styles from "../../../layout.module.scss";
 import { Image, Link } from "@mittwald/flow-react-components";
 import logoMittwald from "../../../../../assets/flow-logo.svg";
 import HeaderNavigation from "@/app/_components/layout/HeaderNavigation";
 import { MdxFile, type SerializedMdxFile } from "@/lib/mdx/MdxFile";
 import MobileNavigation from "@/app/_components/layout/MobileNavigation";
+import clsx from "clsx";
 
 interface Props {
   docs: SerializedMdxFile[];
 }
 
-const Header: FC<Props> = async (props) => {
+const Header: FC<Props> = (props) => {
   const docs = props.docs.map(MdxFile.deserialize);
 
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setHasScrolled(true);
+      } else {
+        setHasScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={clsx(styles.header, hasScrolled && styles.scrolled)}>
       <div className={styles.headerContent}>
         <Link href="/">
           <Image
