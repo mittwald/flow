b4237d8c107bf11a037ca52ccc898c69a62b2c08
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-11-27 06:52:52 +0100

    2131 extend the color component (#2148)

--- packages/components/src/components/Color/Color.tsx
+++ packages/components/src/components/Color/Color.tsx
@@ -1,17 +1,42 @@
 import type { FC, PropsWithChildren } from "react";
 import styles from "./Color.module.scss";
 
+const flowColors = [
+  "blue",
+  "violet",
+  "teal",
+  "lilac",
+  "danger",
+  "warning",
+  "info",
+  "success",
+] as const;
+
+type FlowColor = (typeof flowColors)[number];
+
+function isFlowColor(something: unknown): something is FlowColor {
+  const anyFlowColors = flowColors as readonly string[];
+  return typeof something === "string" && anyFlowColors.includes(something);
+}
+
 export interface ColorProps extends PropsWithChildren {
   /** The color of the element. @default "blue" */
-  color?: "blue" | "violet" | "teal" | "lilac";
+  color?: FlowColor | (string & {});
 }
 
 /** @flr-generate all */
-
 export const Color: FC<ColorProps> = (props) => {
   const { children, color = "blue" } = props;
 
-  return <span className={color ? styles[color] : undefined}>{children}</span>;
+  const isAFlowColor = isFlowColor(color);
+  const className = isAFlowColor ? styles[color] : undefined;
+  const style = !isAFlowColor ? { color } : undefined;
+
+  return (
+    <span className={className} style={style}>
+      {children}
+    </span>
+  );
 };
 
 export default Color;
