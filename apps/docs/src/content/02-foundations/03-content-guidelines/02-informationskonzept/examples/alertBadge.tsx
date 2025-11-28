a00a5028b232828b3a335e917495fafcfba96378
lstockmann <l.stockmann@mittwald.de>
2025-11-28 11:38:26 +0100

    docs: add alert examples

---
+++ apps/docs/src/content/02-foundations/03-content-guidelines/02-informationskonzept/examples/alertBadge.tsx
@@ -0,0 +1,11 @@
+import { AlertBadge } from "@mittwald/flow-react-components";
+
+<Row>
+  <AlertBadge>Info</AlertBadge>
+
+  <AlertBadge status="warning">Warning</AlertBadge>
+
+  <AlertBadge status="danger">Danger</AlertBadge>
+
+  <AlertBadge status="success">Success</AlertBadge>
+</Row>;
