a1350f7aa6a11c6842cdb90293ed8efc0ad12a58
Jan-Eimertenbrink <j.eimertenbrink@mittwald.de>
2025-11-28 11:03:19 +0100

    docs: update color component

---
+++ apps/docs/src/content/03-components/content/color/icon.tsx
@@ -0,0 +1,17 @@
+import {
+    Color,
+    Icon,
+} from "@mittwald/flow-react-components";
+
+<Row>
+    <Color color="success">
+        <Icon>
+            <arrow-up-right />
+        </Icon>
+    </Color>
+    <Color color="danger">
+        <Icon>
+            <arrow-down-right />
+        </Icon>
+    </Color>
+</Row>;
