2645006c8b9d22701762c632921d3d051a5cc514
lstockmann <l.stockmann@mittwald.de>
2025-12-09 12:45:21 +0100

    add accent box tests

---
+++ packages/remote-react-components/src/tests/visual/AccentBox.browser.test.tsx
@@ -0,0 +1,63 @@
+import { expect, test } from "vitest";
+import { testEnvironments } from "@/tests/lib/environments";
+import { LayoutCard } from "@/auto-generated";
+
+type AccentBoxColor = "blue" | "green" | "gradient" | "neutral";
+
+const accentBoxColors: AccentBoxColor[] = [
+  "blue",
+  "green",
+  "gradient",
+  "neutral",
+];
+
+test.each(testEnvironments)(
+  "AccentBox colors (%s)",
+  async ({
+    container,
+    render,
+    components: { Flex, AccentBox, Section, Heading, Text, Link, IconStar },
+  }) => {
+    await render(
+      <Flex direction="column" gap="m">
+        {accentBoxColors.map((color) => (
+          <AccentBox key={color} color={color}>
+            <IconStar />
+            <Section>
+              <Heading>Heading</Heading>
+              <Text>Text</Text>
+              <Link>Link</Link>
+            </Section>
+          </AccentBox>
+        ))}
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("AccentBox colors - default");
+  },
+);
+
+test.each(testEnvironments)(
+  "AccentBox in LayoutCard (%s)",
+  async ({
+    container,
+    render,
+    components: { AccentBox, Section, Heading, Text, Link },
+  }) => {
+    await render(
+      <LayoutCard>
+        <AccentBox>
+          <Section>
+            <Heading>Heading</Heading>
+            <Text>Text</Text>
+            <Link>Link</Link>
+          </Section>
+        </AccentBox>
+      </LayoutCard>,
+    );
+
+    await expect(container).toMatchScreenshot(
+      "AccentBox in LayoutCard - default",
+    );
+  },
+);
