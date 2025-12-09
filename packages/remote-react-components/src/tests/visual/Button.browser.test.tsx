0fd0f25d4a2c67edbf7ef81e40c13e4a8de5d89b
lstockmann <l.stockmann@mittwald.de>
2025-12-09 11:42:29 +0100

    update button tests

--- packages/remote-react-components/src/tests/visual/Button.browser.test.tsx
+++ packages/remote-react-components/src/tests/visual/Button.browser.test.tsx
@@ -2,15 +2,79 @@ import { testEnvironments } from "@/tests/lib/environments";
 import { expect, test } from "vitest";
 import { page, userEvent } from "vitest/browser";
 
+type ButtonColor =
+  | "primary"
+  | "accent"
+  | "danger"
+  | "secondary"
+  | "dark"
+  | "light";
+
+const buttonColors: ButtonColor[] = [
+  "primary",
+  "accent",
+  "danger",
+  "secondary",
+  "dark",
+  "light",
+];
+
+type ButtonVariant = "solid" | "outline" | "soft" | "plain";
+
+const buttonVariants: ButtonVariant[] = ["solid", "outline", "soft", "plain"];
+
+const firstLetterToUppercase = (text: string) => {
+  return text[0]!.toUpperCase() + text.slice(1).toLowerCase();
+};
+
 test.each(testEnvironments)(
   "Button states (%s)",
-  async ({ container, render, components: { Flex, Button } }) => {
+  async ({
+    container,
+    render,
+    components: { Flex, Button, IconInfo, Text },
+  }) => {
     await render(
-      <Flex gap="s">
-        <Button>Default</Button>
-        <Button isDisabled>Disabled</Button>
-        <Button data-testid="hover">Hover</Button>
-        <Button>Focus</Button>
+      <Flex direction="column" gap="m">
+        <Flex gap="s">
+          <Button>Default</Button>
+          <Button data-testid="hover">Hover</Button>
+          <Button>Focus</Button>
+          <Button>
+            <IconInfo />
+          </Button>
+          <Button>
+            <IconInfo />
+            <Text>Icon & Text</Text>
+          </Button>
+        </Flex>
+        <Flex gap="s">
+          <Button size="s">Small</Button>
+          <Button size="s">
+            <IconInfo />
+          </Button>
+          <Button size="s">
+            <IconInfo />
+            <Text>Icon & Text</Text>
+          </Button>
+        </Flex>
+
+        {buttonVariants.map((variant) => (
+          <Flex gap="s" key={variant}>
+            <Button variant={variant} isPending>
+              {firstLetterToUppercase(variant)} Pending
+            </Button>
+            <Button variant={variant} isSucceeded>
+              {firstLetterToUppercase(variant)} Succeeded
+            </Button>
+            <Button variant={variant} isFailed>
+              {firstLetterToUppercase(variant)} Failed
+            </Button>
+            <Button variant={variant} isDisabled>
+              {firstLetterToUppercase(variant)} Disabled
+            </Button>
+          </Flex>
+        ))}
       </Flex>,
     );
 
@@ -23,5 +87,85 @@ test.each(testEnvironments)(
     await userEvent.tab();
 
     await expect(container).toMatchScreenshot("Button states - default");
+  },
+);
+
+test.each(testEnvironments)(
+  "Button colors (%s)",
+  async ({
+    container,
+    render,
+    components: { Flex, Button, AccentBox, Wrap },
+  }) => {
+    await render(
+      <Flex direction="column" gap="m">
+        {buttonColors.map((color) => (
+          <Wrap if={color === "light"} key={color}>
+            <AccentBox>
+              <Flex gap="s" key={color}>
+                {buttonVariants.map((variant) => (
+                  <Button variant={variant} color={color} key={variant}>
+                    {firstLetterToUppercase(color)}{" "}
+                    {firstLetterToUppercase(variant)}
+                  </Button>
+                ))}
+              </Flex>
+            </AccentBox>
+          </Wrap>
+        ))}
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("Button colors - default");
+  },
+);
+
+test.each(testEnvironments)(
+  "Button with Avatar (%s)",
+  async ({
+    container,
+    render,
+    components: { Avatar, Button, Initials, Flex },
+  }) => {
+    await render(
+      <Flex gap="s">
+        <Button>
+          <Avatar>
+            <Initials>Max Mustermann</Initials>
+          </Avatar>
+        </Button>
+        <Button data-testid="hover">
+          <Avatar>
+            <Initials>Max Mustermann</Initials>
+          </Avatar>
+        </Button>
+        <Button>
+          <Avatar>
+            <Initials>Max Mustermann</Initials>
+          </Avatar>
+        </Button>
+      </Flex>,
+    );
+
+    const hoverButton = page.getByTestId("hover");
+    await hoverButton.hover();
+
+    await userEvent.tab();
+    await userEvent.tab();
+    await userEvent.tab();
+
+    await expect(container).toMatchScreenshot("Button with avatar - default");
+  },
+);
+
+test.each(testEnvironments)(
+  "Button pressed (%s)",
+  async ({ container, render, components: { Button } }) => {
+    await render(<Button data-testid="pressed">Pressed</Button>);
+
+    const pressedButton = page.getByTestId("pressed");
+    await pressedButton.click();
+
+    await expect(container).toMatchScreenshot("Button pressed - default");
   },
 );
