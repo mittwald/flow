501d9d7479249eacccb9086a347d65827e3db594
lstockmann <l.stockmann@mittwald.de>
2025-12-10 09:16:51 +0100

    add checkbox tests

--- packages/remote-react-components/src/tests/visual/Checkbox.browser.test.tsx
+++ packages/remote-react-components/src/tests/visual/Checkbox.browser.test.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { userEvent } from "vitest/browser";
 
 test.each(testEnvironments)(
-  "Checkbox (%s)",
+  "Checkbox states (%s)",
   async ({ container, render, components: { Flex, Checkbox } }) => {
     await render(
       <Flex direction="column" gap="m">
@@ -23,6 +23,26 @@ test.each(testEnvironments)(
     await userEvent.tab();
     await userEvent.tab();
 
-    await expect(container).toMatchScreenshot("Chat");
+    await expect(container).toMatchScreenshot("Checkbox states");
+  },
+);
+
+test.each(testEnvironments)(
+  "Checkbox edge cases(%s)",
+  async ({ container, render, components: { Checkbox } }) => {
+    await render(
+      <Checkbox>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+        quam quas vel voluptas, ullam aliquid fugit. Voluptate harum accusantium
+        rerum ullam modi blanditiis vitae, laborum ea tempore, dolore voluptas.
+        Earum pariatur, similique corrupti id officia perferendis. Labore,
+        similique.
+      </Checkbox>,
+    );
+
+    await userEvent.tab();
+    await userEvent.tab();
+
+    await expect(container).toMatchScreenshot("Checkbox edge cases");
   },
 );
