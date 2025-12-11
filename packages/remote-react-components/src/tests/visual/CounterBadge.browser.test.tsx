8a3c3eb3eba1f67086aabd6f2c740027e356b4a3
lstockmann <l.stockmann@mittwald.de>
2025-12-11 07:21:45 +0100

    add counter badge tests

---
+++ packages/remote-react-components/src/tests/visual/CounterBadge.browser.test.tsx
@@ -0,0 +1,25 @@
+import { testEnvironments } from "@/tests/lib/environments";
+import { expect, test } from "vitest";
+import React from "react";
+
+test.each(testEnvironments)(
+  "CounterBadge (%s)",
+  async ({
+    container,
+    render,
+    components: { Flex, CounterBadge, IconNotification, Button },
+  }) => {
+    await render(
+      <Flex gap="s">
+        <CounterBadge />
+        <CounterBadge count={120} />
+        <Button aria-label="Count: 7">
+          <IconNotification />
+          <CounterBadge count={7} />
+        </Button>
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("CounterBadge");
+  },
+);
