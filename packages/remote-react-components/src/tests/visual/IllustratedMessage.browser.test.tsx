4698eda43d05be8599b4ae5942861b0cb418ca7b
lstockmann <l.stockmann@mittwald.de>
2025-12-11 14:33:44 +0100

    add illustrated message tests

---
+++ packages/remote-react-components/src/tests/visual/IllustratedMessage.browser.test.tsx
@@ -0,0 +1,87 @@
+import { testEnvironments } from "@/tests/lib/environments";
+import { expect, test } from "vitest";
+import React from "react";
+
+const illustratedMessageColors = [
+  "primary",
+  "danger",
+  "dark",
+  "light",
+] as const;
+
+test.each(testEnvironments)(
+  "IllustratedMessage colors (%s)",
+  async ({
+    container,
+    render,
+    components: {
+      Heading,
+      Flex,
+      IconStar,
+      IllustratedMessage,
+      Text,
+      Button,
+      Wrap,
+      AccentBox,
+      ActionGroup,
+      ProgressBar,
+      Label,
+    },
+  }) => {
+    await render(
+      <Flex gap="s" align="center">
+        {illustratedMessageColors.map((color) => (
+          <Wrap if={color === "light"} key={color}>
+            <AccentBox>
+              <IllustratedMessage color={color}>
+                <IconStar />
+                <Heading>Heading</Heading>
+                <Text>Text</Text>
+                <Button>Button</Button>
+                <ProgressBar value={75}>
+                  <Label>Label</Label>
+                </ProgressBar>
+                <ActionGroup>
+                  <Button variant="soft" color="secondary">
+                    Secondary
+                  </Button>
+                  <Button>Primary</Button>
+                </ActionGroup>
+              </IllustratedMessage>
+            </AccentBox>
+          </Wrap>
+        ))}
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("IllustratedMessage colors");
+  },
+);
+
+test.each(testEnvironments)(
+  "IllustratedMessage edge cases (%s)",
+  async ({
+    container,
+    render,
+    components: { Heading, IllustratedMessage, Text },
+  }) => {
+    await render(
+      <IllustratedMessage>
+        <Heading>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+          quam quas vel voluptas, ullam aliquid fugit. Voluptate harum
+          accusantium rerum ullam modi blanditiis vitae.
+        </Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+          quam quas vel voluptas, ullam aliquid fugit. Voluptate harum
+          accusantium rerum ullam modi blanditiis vitae, laborum ea tempore,
+          dolore voluptas. Earum pariatur, similique corrupti id officia
+          perferendis. Labore, similique.
+        </Text>
+      </IllustratedMessage>,
+    );
+
+    await expect(container).toMatchScreenshot("IllustratedMessage edge cases");
+  },
+);
