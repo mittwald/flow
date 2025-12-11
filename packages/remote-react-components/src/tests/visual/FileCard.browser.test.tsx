caa74e77083cec73a3d47623e88c06d7d4421fe7
lstockmann <l.stockmann@mittwald.de>
2025-12-11 10:57:01 +0100

    add file card list tests

--- packages/remote-react-components/src/tests/visual/FileCard.browser.test.tsx
+++ packages/remote-react-components/src/tests/visual/FileCard.browser.test.tsx
@@ -1,7 +1,6 @@
 import { testEnvironments } from "@/tests/lib/environments";
 import { expect, test } from "vitest";
 import gopher from "@/tests/assets/gopher.webp";
-import React from "react";
 
 test.each(testEnvironments)(
   "FileCard variants (%s)",
