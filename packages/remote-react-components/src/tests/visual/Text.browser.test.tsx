b546d7b3fc21f17bde898fad3e6156d9b80faf20
lstockmann <l.stockmann@mittwald.de>
2025-12-12 12:01:33 +0100

    add text tests

---
+++ packages/remote-react-components/src/tests/visual/Text.browser.test.tsx
@@ -0,0 +1,47 @@
+import { testEnvironments } from "@/tests/lib/environments";
+import { expect, test } from "vitest";
+import React from "react";
+
+test.each(testEnvironments)(
+  "Text (%s)",
+  async ({ container, render, components: { Text, Flex, AccentBox } }) => {
+    await render(
+      <Flex direction="column" gap="m">
+        <Text>
+          Lorem ipsum <strong>dolor sit</strong> amet consectetur
+          <i>adipisicing</i> elit. Cumque eius <s>quam quas</s> vel voluptas,
+          ullam aliquid fugit. Voluptate harum accusantium rerum ullam modi
+          blanditiis vitae
+          <br />
+          <small>laborum ea tempore, dolore voluptas.</small>
+          <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ul>
+          <ol>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ol>
+        </Text>
+        <Text color="dark">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+          quam quas vel voluptas, ullam aliquid fugit. Voluptate harum
+          accusantium rerum ullam modi blanditiis vitae, laborum ea tempore,
+          dolore voluptas.
+        </Text>
+        <AccentBox>
+          <Text color="light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+            quam quas vel voluptas, ullam aliquid fugit. Voluptate harum
+            accusantium rerum ullam modi blanditiis vitae, laborum ea tempore,
+            dolore voluptas.
+          </Text>
+        </AccentBox>
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("Text");
+  },
+);
