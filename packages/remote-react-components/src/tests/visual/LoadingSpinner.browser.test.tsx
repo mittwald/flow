5acdc6226eca0fcacbfaa350ab122c6b8f2d340a
lstockmann <l.stockmann@mittwald.de>
2025-12-12 06:09:27 +0100

    add loading spinner tests

---
+++ packages/remote-react-components/src/tests/visual/LoadingSpinner.browser.test.tsx
@@ -0,0 +1,32 @@
+import { testEnvironments } from "@/tests/lib/environments";
+import { expect, test } from "vitest";
+
+const colors = ["default", "dark", "light"] as const;
+const sizes = ["s", "m", "l"] as const;
+
+test.each(testEnvironments)(
+  "LoadingSpinner (%s)",
+  async ({
+    container,
+    render,
+    components: { LoadingSpinner, Flex, Wrap, AccentBox },
+  }) => {
+    await render(
+      <Flex gap="m" direction="column">
+        {colors.map((color) => (
+          <Wrap if={color === "light"} key={color}>
+            <AccentBox>
+              <Flex gap="s">
+                {sizes.map((size) => (
+                  <LoadingSpinner size={size} color={color} />
+                ))}
+              </Flex>
+            </AccentBox>
+          </Wrap>
+        ))}
+      </Flex>,
+    );
+
+    await expect(container).toMatchScreenshot("LoadingSpinner");
+  },
+);
