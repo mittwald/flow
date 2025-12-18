9224c719b81bbbf97bae7491ad86943815f54c29
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2025-12-18 09:44:38 +0100

    docs: fix composition example

--- apps/docs/src/content/01-get-started/stylesheet/examples/composition.tsx
+++ apps/docs/src/content/01-get-started/stylesheet/examples/composition.tsx
@@ -1,9 +1,11 @@
 import ExampleSvg from "@/content/01-get-started/stylesheet/examples/components/ExampleSvg";
 
 <aside className="flow--alert">
-  <h3 className="flow--heading flow--heading--h3 flow--alert--heading">
-    <ExampleSvg className="flow--icon flow--alert-icon" />
-    E-Mail Adresse wurde archiviert
+  <h3 className="flow--heading flow--heading--s flow--alert--heading">
+    <span className="flow--heading--heading-text">
+      <ExampleSvg className="flow--icon flow--alert-icon flow--heading--icon" />
+      E-Mail-Adresse wurde archiviert
+    </span>
   </h3>
   <div className="flow--alert--content">
     Da deine Domain gelöscht wurde, wurde diese
