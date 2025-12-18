f12e449d62135ee88cdc79248ea4dbe50c9aa574
lstockmann <l.stockmann@mittwald.de>
2025-12-18 09:34:16 +0100

    docs: fix composition example

--- apps/docs/src/content/01-get-started/stylesheet/examples/composition.tsx
+++ apps/docs/src/content/01-get-started/stylesheet/examples/composition.tsx
@@ -3,7 +3,7 @@ import ExampleSvg from "@/content/01-get-started/stylesheet/examples/components/
 <aside className="flow--alert">
   <h3 className="flow--heading flow--heading--s flow--alert--heading">
     <span className="flow--heading--heading-text">
-      <ExampleSvg className="flow--icon flow--alert-icon flow--heading--icon " />
+      <ExampleSvg className="flow--icon flow--alert-icon flow--heading--icon" />
       E-Mail-Adresse wurde archiviert
     </span>
   </h3>
