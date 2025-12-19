eb85bb433057d94d3bb5cbed0ba8d96b2ef3dff0
kati-ux <k.possenriede@mittwald.de>
2025-12-19 11:29:53 +0100

    docs: Update alertBadge.tsx

--- apps/docs/src/content/02-foundations/03-content-guidelines/02-informationskonzept/examples/alertBadge.tsx
+++ apps/docs/src/content/02-foundations/03-content-guidelines/02-informationskonzept/examples/alertBadge.tsx
@@ -3,9 +3,13 @@ import { AlertBadge } from "@mittwald/flow-react-components";
 <Row>
   <AlertBadge>Update verfügbar</AlertBadge>
 
-  <AlertBadge status="warning">Speicherplatz fast voll</AlertBadge>
+  <AlertBadge status="warning">
+    Speicherplatz fast voll
+  </AlertBadge>
 
-  <AlertBadge status="danger">Speicherplatz voll</AlertBadge>
+  <AlertBadge status="danger">
+    Speicherplatz voll
+  </AlertBadge>
 
   <AlertBadge status="success">Verifiziert</AlertBadge>
 </Row>;
