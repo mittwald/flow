877de7907d841578b7a9544829f1e87649988564
lstockmann <l.stockmann@mittwald.de>
2026-01-15 07:16:21 +0100

    docs: update boundary docs

--- apps/docs/src/content/02-foundations/04-internal/01-boundaries/examples/content-fragment-loading.tsx
+++ apps/docs/src/content/02-foundations/04-internal/01-boundaries/examples/content-fragment-loading.tsx
@@ -1,14 +1,13 @@
 import {
-  Heading,
   Section,
   SkeletonText,
   Text,
 } from "@mittwald/flow-react-components";
 
 <Section>
-  <Heading>
-    <SkeletonText width="35%" />
-  </Heading>
+  <Text>
+    <SkeletonText />
+  </Text>
   <Text>
     <SkeletonText />
   </Text>
