65b07e9aaf660198c74127eeedf47e09a9fa4339
lstockmann <l.stockmann@mittwald.de>
2026-01-15 07:25:20 +0100

    docs: update boundary docs

--- apps/docs/src/content/02-foundations/04-internal/01-boundaries/examples/modal-error.tsx
+++ apps/docs/src/content/02-foundations/04-internal/01-boundaries/examples/modal-error.tsx
@@ -7,20 +7,28 @@ import {
   Text,
 } from "@mittwald/flow-react-components";
 
-<StaticModal>
-  <div className="flow--modal--content">
-    <IllustratedMessage color="danger">
-      <AlertIcon status="danger" />
-      <Heading>Fehler beim Laden </Heading>
-      <Text>
-        Dieser Bereich konnte nicht geladen werden. Wir
-        arbeiten daran das Problem zu beheben.
-      </Text>
-    </IllustratedMessage>
-  </div>
-  <ActionGroup className="flow--modal--action-group">
-    <Button color="secondary" variant="soft">
-      Schließen
-    </Button>
-  </ActionGroup>
-</StaticModal>;
+<div
+  style={{
+    background: "var(--overlay--background-color)",
+    margin: "calc(var(--size-px--l) * -1)",
+    padding: "var(--size-px--l)",
+  }}
+>
+  <StaticModal>
+    <div className="flow--modal--content">
+      <IllustratedMessage color="danger">
+        <AlertIcon status="danger" />
+        <Heading>Fehler beim Laden </Heading>
+        <Text>
+          Dieser Bereich konnte nicht geladen werden. Wir
+          arbeiten daran das Problem zu beheben.
+        </Text>
+      </IllustratedMessage>
+    </div>
+    <ActionGroup className="flow--modal--action-group">
+      <Button color="secondary" variant="soft">
+        Schließen
+      </Button>
+    </ActionGroup>
+  </StaticModal>
+</div>;
