c3c77cfec327647b924ebfe440fbfd99db9e5891
Jan Eimertenbrink <166510726+Jan-Eimertenbrink@users.noreply.github.com>
2026-01-20 15:09:03 +0100

    docs: Add Ui-Pattern Error Handling (#2234)
    * docs: Add Ui-Pattern Error Handling
    
    * Update overview.mdx
    
    Add Links
    
    * docs: add error examples
    
    * add link to Forms (Validation) UiPattern
    
    * docs: update error docs
    
    * docs: update error docs
    
    * docs: update error docs
    
    * Update Index after Review
    
    * Update overview.mdx after review
    
    * Update index.mdx
    
    * Update overview.mdx
    
    * Update overview.mdx
    
    ---------
    
    Co-authored-by: lstockmann <l.stockmann@mittwald.de>

---
+++ apps/docs/src/content/02-foundations/05-ui-patterns/05-errorhandling/examples/illustrated-message.tsx
@@ -0,0 +1,31 @@
+import {
+  Flex,
+  Heading,
+  IconDanger,
+  IllustratedMessage,
+  LayoutCard,
+  Text,
+} from "@mittwald/flow-react-components";
+
+export default () => {
+  return (
+    <Flex direction="column" gap="m">
+      <Heading level={1} color="light">
+        Mein Projekt
+      </Heading>
+
+      <LayoutCard>
+        <IllustratedMessage color="danger">
+          <IconDanger />
+          <Heading>Fehler beim Laden von Daten</Heading>
+          <Text>
+            Dieser Bereich konnte nicht geladen werden. Wir
+            arbeiten daran das Problem zu beheben. Bitte
+            habe etwas Geduld und probiere es später noch
+            einmal.
+          </Text>
+        </IllustratedMessage>
+      </LayoutCard>
+    </Flex>
+  );
+};
