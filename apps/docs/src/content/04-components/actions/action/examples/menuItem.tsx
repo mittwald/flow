aec9942d2607c3bfd667ad43c96f5605ff20cb20
lstockmann <l.stockmann@mittwald.de>
2026-01-27 14:42:01 +0100

    merge main

--- apps/docs/src/content/04-components/actions/action/examples/menuItem.tsx
+++ apps/docs/src/content/04-components/actions/action/examples/menuItem.tsx
@@ -5,7 +5,7 @@ import {
   ContextMenu,
   MenuItem,
 } from "@mittwald/flow-react-components";
-import { sleepLong } from "@/content/03-components/actions/action/examples/lib";
+import { sleepLong } from "@/content/04-components/actions/action/examples/lib";
 
 <ContextMenuTrigger>
   <Button>Trigger</Button>
