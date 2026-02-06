3c656a3c02dc5322f5d09af09b9906215b4824d1
lstockmann <l.stockmann@mittwald.de>
2026-02-06 07:36:28 +0100

    feat(Truncate) add truncate component

--- packages/remote-react-components/src/tests/visual/Truncate.browser.test.tsx
+++ packages/remote-react-components/src/tests/visual/Truncate.browser.test.tsx
@@ -13,20 +13,26 @@ test.each(testEnvironments)(
         <Truncate>
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua.
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
         </Truncate>
         <Text>
           <Truncate>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua.
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
           </Truncate>
         </Text>
         <Heading>
           <Truncate>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua.
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet.
           </Truncate>
         </Heading>
       </Flex>,
