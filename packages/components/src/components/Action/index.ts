8afe49a49cc1d5c9f9665bc3861a33deb964f5a5
Marco Falkenberg <m.falkenberg@mittwald.de>
2026-02-11 08:45:19 +0100

    feat(ActionBatch): add ActionBatch to explicitly create batches

--- packages/components/src/components/Action/index.ts
+++ packages/components/src/components/Action/index.ts
@@ -1,5 +1,6 @@
 export { Action } from "./Action";
 export { MutedActionError } from "./MutedActionError";
+export { ActionBatch } from "./ActionBatch";
 export * from "./types";
 export { default } from "./Action";
 export { useAriaAnnounceSuspense } from "./lib/ariaLive";
