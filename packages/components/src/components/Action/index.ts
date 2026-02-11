4b1cbcf90cd45e5342507daeb5bc3fc8040722d1
Marco Falkenberg <m.falkenberg@mittwald.de>
2026-02-11 08:44:12 +0100

    feat(Action): allow Action to fail without throwing error

--- packages/components/src/components/Action/index.ts
+++ packages/components/src/components/Action/index.ts
@@ -1,4 +1,5 @@
 export { Action } from "./Action";
+export { MutedActionError } from "./MutedActionError";
 export * from "./types";
 export { default } from "./Action";
 export { useAriaAnnounceSuspense } from "./lib/ariaLive";
