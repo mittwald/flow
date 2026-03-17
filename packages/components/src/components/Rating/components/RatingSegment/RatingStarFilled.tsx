81eef19b3004ea92cae02c68c344102ffbe7cc61
lstockmann <l.stockmann@mittwald.de>
2026-03-17 11:52:53 +0100

    fix(Icon): remove solid icons

--- packages/components/src/components/Rating/components/RatingSegment/RatingStarFilled.tsx
+++ packages/components/src/components/Rating/components/RatingSegment/RatingStarFilled.tsx
@@ -13,11 +13,13 @@ export const RatingStarFilled: FC<Props> = (props) => {
   return (
     <Icon aria-hidden size={size} className={styles.starFilled}>
       <svg
+        width="21"
+        height="20"
+        viewBox="0 0 21 20"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 640 640"
       >
-        <path d="M320.2 11.2L227.6 192.6L26.5 224.6L170.4 368.7L138.7 569.9L320.2 477.6L501.7 569.9L470 368.7L613.9 224.6L412.8 192.6L320.2 11.2z" />
+        <path d="M10.4614 0L13.5003 6.81738L20.923 7.60081L15.3784 12.5976L16.9271 19.8992L10.4614 16.17L3.99579 19.8992L5.54446 12.5976L-0.000195503 7.60081L7.42258 6.81738L10.4614 0Z" />
       </svg>
     </Icon>
   );
