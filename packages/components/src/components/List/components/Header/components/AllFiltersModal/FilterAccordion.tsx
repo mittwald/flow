65c012ccca81ca57a639e660823d84230612d186
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2026-03-20 08:01:34 +0100

    feat(List): add radio filter reset button (#2376)

--- packages/components/src/components/List/components/Header/components/AllFiltersModal/FilterAccordion.tsx
+++ packages/components/src/components/List/components/Header/components/AllFiltersModal/FilterAccordion.tsx
@@ -7,6 +7,10 @@ import RadioView from "@/views/RadioView";
 import CheckboxGroupView from "@/views/CheckboxGroupView";
 import CheckboxView from "@/views/CheckboxView";
 import type { Filter } from "@/components/List/model/filter/Filter";
+import ButtonView from "@/views/ButtonView";
+import { Flex } from "@/components/Flex";
+import { useLocalizedStringFormatter } from "react-aria";
+import locales from "../../../../locales/*.locale.json";
 
 interface Props {
   filter: Filter<never, never, never>;
@@ -17,29 +21,50 @@ export const FilterAccordion: FC<Props> = (props) => {
 
   const activeKeys = filter.values.filter((v) => v.isActive).map((v) => v.id);
 
+  const name = filter.name ?? filter.property;
+
+  const stringFormatter = useLocalizedStringFormatter(locales);
+
   return (
     <AccordionView>
-      <HeadingView>{filter.name ?? filter.property}</HeadingView>
+      <HeadingView>{name}</HeadingView>
       <ContentView>
         {filter.mode === "one" && (
-          <RadioGroupView value={activeKeys[0]} m={[1, 1]}>
-            {filter.values.map((v) => (
-              <RadioView
-                key={v.id}
-                value={v.id}
-                onPress={() => {
-                  if (!v.isActive) {
-                    v.toggle();
-                  }
-                }}
+          <Flex direction="column" gap="m">
+            <RadioGroupView
+              value={activeKeys[0]}
+              m={[1, 1]}
+              key={activeKeys[0]}
+              aria-label={name}
+            >
+              {filter.values.map((v) => (
+                <RadioView
+                  key={v.id}
+                  value={v.id}
+                  onPress={() => {
+                    if (!v.isActive) {
+                      v.toggle();
+                    }
+                  }}
+                >
+                  {v.render()}
+                </RadioView>
+              ))}
+            </RadioGroupView>
+            {activeKeys.length > 0 && (
+              <ButtonView
+                size="s"
+                color="secondary"
+                variant="soft"
+                onPress={() => filter.clear()}
               >
-                {v.render()}
-              </RadioView>
-            ))}
-          </RadioGroupView>
+                {stringFormatter.format("list.filters.clearSelection")}
+              </ButtonView>
+            )}
+          </Flex>
         )}
         {filter.mode !== "one" && (
-          <CheckboxGroupView value={activeKeys} m={[1, 1]}>
+          <CheckboxGroupView value={activeKeys} m={[1, 1]} aria-label={name}>
             {filter.values.map((v) => (
               <CheckboxView key={v.id} value={v.id} onPress={() => v.toggle()}>
                 {v.render()}
