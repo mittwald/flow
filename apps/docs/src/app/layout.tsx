ecff228298faa1e43dff422e3d25b966d996b50f
lstockmann <l.stockmann@mittwald.de>
2026-03-23 11:56:52 +0100

    add background

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -6,13 +6,14 @@ import clsx from "clsx";
 import styles from "./layout.module.scss";
 import { MdxFileFactory } from "@/lib/mdx/MdxFileFactory";
 import MainNavigation from "@/app/_components/layout/MainNavigation";
-import { NotificationProvider } from "@mittwald/flow-react-components";
 import { Matomo } from "@/app/_components/Matomo";
 import { RouterProvider } from "@mittwald/flow-react-components/nextjs";
 import Header from "@/app/_components/layout/Header";
 import ScrollToHash from "@/app/_components/ScrollToHash";
 import Footer from "@/app/_components/layout/Footer";
 import { ThemeProvider } from "next-themes";
+import { NotificationProvider } from "@mittwald/flow-react-components";
+import bg from "../../public/assets/bg.png";
 
 export const metadata: Metadata = {
   title: "Flow – mittwald Design System",
@@ -26,6 +27,10 @@ const RootLayout: FC<PropsWithChildren> = async (props) => {
   return (
     <html lang="de" suppressHydrationWarning>
       <body className={bodyClassName}>
+        <div
+          style={{ backgroundImage: `url(${bg.src})` }}
+          className={styles.background}
+        />
         <ThemeProvider attribute="data-flow-theme" disableTransitionOnChange>
           <RouterProvider>
             <ScrollToHash />
