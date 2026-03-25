30ddf3431f5a135973570f48126b30ce52f468b1
Marco Rieger <m.rieger@mittwald.de>
2026-03-25 14:07:37 +0100

    add screenshot tests

---
+++ packages/remote-react-components/src/tests/visual/CodeEditor.browser.test.tsx
@@ -0,0 +1,21 @@
+import { testEnvironments } from "@/tests/lib/environments";
+import { test } from "vitest";
+
+test.each(testEnvironments)(
+  "CodeEditor (%s)",
+  async ({ testScreenshot, render, components: { CodeEditor, Flex } }) => {
+    await render(
+      <Flex direction="column" gap="m">
+        <CodeEditor
+          language="json"
+          value={`{
+    "projectId": "b3a96db5-ba8f-40dd-9100-bab43ac1f698",
+    "name": "My Project"
+}`}
+        />
+      </Flex>,
+    );
+
+    await testScreenshot("CodeBlock");
+  },
+);
