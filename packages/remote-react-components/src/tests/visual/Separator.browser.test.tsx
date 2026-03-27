e0a671a8cc13d72617c3c721324205f1f13df1e0
Lisa Stockmann <84317589+Lisa18289@users.noreply.github.com>
2026-03-27 13:07:59 +0100

    feat(Separator): allow vertical separator (#2410)

--- packages/remote-react-components/src/tests/visual/Separator.browser.test.tsx
+++ packages/remote-react-components/src/tests/visual/Separator.browser.test.tsx
@@ -1,10 +1,44 @@
 import { testEnvironments } from "@/tests/lib/environments";
 import { test } from "vitest";
+import React from "react";
 
 test.each(testEnvironments)(
   "Separator (%s)",
-  async ({ testScreenshot, render, components: { Separator } }) => {
-    await render(<Separator />);
+  async ({ testScreenshot, render, components: { Separator, Flex, Text } }) => {
+    await render(
+      <Flex direction="column" gap="xl">
+        <Flex gap="m" direction="column">
+          <Text align="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+            quam quas vel voluptas, ullam aliquid fugit. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Cumque eius quam quas vel
+            voluptas, ullam aliquid fugit.
+          </Text>
+          <Separator />
+          <Text align="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+            quam quas vel voluptas, ullam aliquid fugit. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Cumque eius quam quas vel
+            voluptas, ullam aliquid fugit.
+          </Text>
+        </Flex>
+        <Flex gap="m">
+          <Text align="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+            quam quas vel voluptas, ullam aliquid fugit. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Cumque eius quam quas vel
+            voluptas, ullam aliquid fugit.
+          </Text>
+          <Separator orientation="vertical" />
+          <Text align="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius
+            quam quas vel voluptas, ullam aliquid fugit. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Cumque eius quam quas vel
+            voluptas, ullam aliquid fugit.
+          </Text>
+        </Flex>
+      </Flex>,
+    );
 
     await testScreenshot("Separator");
   },
