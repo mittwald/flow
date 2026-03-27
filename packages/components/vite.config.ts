0b7aab6dd669159ac9856a5db7e725d5dd1ec98b
Marco Rieger <m.rieger@mittwald.de>
2026-03-27 09:49:40 +0100

    dont optimize lezer

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
       },
     ],
   },
+  optimizeDeps: {
+    exclude: ["@lezer/lr"],
+  },
   css: {
     modules: {
       generateScopedName: cssModuleClassNameGenerator,
