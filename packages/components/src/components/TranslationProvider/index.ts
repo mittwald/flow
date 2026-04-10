3cc957870fd310c17fdd8447dd41ff76caa2c355
Marco Rieger <m.rieger@mittwald.de>
2026-04-10 13:16:17 +0200

    export props

--- packages/components/src/components/TranslationProvider/index.ts
+++ packages/components/src/components/TranslationProvider/index.ts
@@ -1,9 +1,10 @@
 export * from "./view";
 export {
+  type TranslationProviderProps,
+  type Translations,
   TranslationProvider,
   useTranslationProvider,
 } from "@/components/TranslationProvider/TranslationProvider";
-export type { Translations } from "@/components/TranslationProvider/TranslationProvider";
 export { useLocalizedStringFormatter } from "@/components/TranslationProvider/useLocalizedStringFormatter";
 
 export { default } from "@/components/TranslationProvider/TranslationProvider";
