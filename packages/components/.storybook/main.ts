5e98865e97bc757d87734bcbb5b97fb9da68d8c1
Marco Rieger <rieger@racecore.de>
2026-04-10 11:23:20 +0200

    Revert "chore(bundle): migrate to vite 8 (#2436)"
    This reverts commit 962e76e87af394559d0a1d78023aea469c2e3f06.

--- packages/components/.storybook/main.ts
+++ packages/components/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 import viteCheckerPlugin from "vite-plugin-checker";
-import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.tsx"],
@@ -15,11 +14,10 @@ const config: StorybookConfig = {
       // no options
     },
   },
-  core: {
-    disableTelemetry: true,
-    builder: "@storybook/builder-vite",
-  },
   viteFinal: async (conf) => {
+    // See why dynamic import: see https://github.com/storybookjs/storybook/issues/26291#issuecomment-1978193283
+    const { mergeConfig } = await import("vite");
+
     return mergeConfig(conf, {
       plugins: [
         viteCheckerPlugin({
