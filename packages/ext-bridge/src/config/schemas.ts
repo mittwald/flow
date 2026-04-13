5288973b3d4ab7b68c9b8295014b014b9e75923e
Marco Falkenberg <m.falkenberg@mittwald.de>
2026-04-13 10:33:40 +0200

    feat(Remote): auto-set host language on remote-side

--- packages/ext-bridge/src/config/schemas.ts
+++ packages/ext-bridge/src/config/schemas.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
 
+/**
+ * Breaking Change warning: Do not remove/rename/modify existing properties in
+ * the config schema, as they might be used by existing extensions.
+ */
 const baseConfig = z.object({
   sessionId: z.string(),
   userId: z.string(),
   extensionId: z.string(),
   extensionInstanceId: z.string(),
   variantKey: z.string().optional().nullable(),
-  language: z.string().default("de"),
 });
 
+/**
+ * Breaking Change warning: Do not remove/rename/modify existing properties in
+ * the config schema, as they might be used by existing extensions.
+ */
 const contextParameters = z.object({
   appInstallationId: z.string().optional(),
   projectId: z.string().optional(),
--- packages/remote-core/src/connection/types.ts
+++ packages/remote-core/src/connection/types.ts
@@ -1,4 +1,8 @@
-import type { ExtBridgeConnectionApi } from "@mittwald/ext-bridge";
+import type {
+  ExtBridgeConfigInput,
+  ExtBridgeConnectionApi,
+} from "@mittwald/ext-bridge";
+import type { HostConfig } from "@mittwald/flow-core";
 import type { RemoteConnection } from "@mittwald/remote-dom-core";
 import type { ThreadIframe, ThreadNestedIframe } from "@quilted/threads";
 export type { RemoteConnection } from "@mittwald/remote-dom-core";
@@ -8,11 +12,30 @@ export interface NavigationState {
   isPending: boolean;
 }
 
-export interface HostExports extends ExtBridgeConnectionApi {
+export type RemoteExtBridgeConfig = Exclude<
+  ExtBridgeConfigInput,
+  keyof HostConfig
+>;
+
+export interface RemoteExtBridgeConnectionApi extends Omit<
+  ExtBridgeConnectionApi,
+  "getConfig"
+> {
+  getConfig: () => Promise<RemoteExtBridgeConfig>;
+}
+
+/**
+ * Breaking Change warning: Do not remove/rename/modify existing properties of
+ * this interface, as they might be used by existing extensions.
+ *
+ * When addding properties, make sure to release the host before all clients.
+ */
+export interface HostExports extends RemoteExtBridgeConnectionApi {
   setIsReady: (version?: Version) => Promise<void>;
   setIsLoading: (isLoading: boolean) => Promise<void>;
   setError: (error: string) => Promise<void>;
   setNavigationState: (state: NavigationState) => Promise<void>;
+  getHostConfig: () => Promise<HostConfig>;
 }
 
 export interface RemoteExports {
