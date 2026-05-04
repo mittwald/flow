d5bbeeb016024bd5bda982fac12afcea195b56b6
Jan Eimertenbrink <166510726+Jan-Eimertenbrink@users.noreply.github.com>
2026-05-04 11:29:21 +0200

    fix example with-content.tsx

--- apps/docs/src/content/04-components/status/alert/examples/with-content.tsx
+++ apps/docs/src/content/04-components/status/alert/examples/with-content.tsx
@@ -8,7 +8,9 @@ import {
 <Alert>
   <Heading>E-Mail-Empfang wurde deaktiviert</Heading>
   <Content>
-    Der Empfang der E-Mails für dieses Postfach ist aufgrund fehlerhafter MX-Records deaktiviert. Bitte setze die richtigen Mailserver. Bei einer Neuregistrierung einer Domain kann es einige Minuten dauern, bis die MX-Records global im DNS verfügbar sind. Die E-Mail-Adresse wird in diesem Fall automatisch aktiviert.
+    Der Empfang der E-Mails für dieses Postfach ist aufgrund fehlerhafter MX-Records deaktiviert. Bitte setze die richtigen Mailserver.
+    Bei einer Neuregistrierung einer Domain kann es einige Minuten dauern, bis die MX-Records global im DNS verfügbar sind. 
+    Die E-Mail-Adresse wird in diesem Fall automatisch aktiviert.
     <Button>MX-Records anpassen</Button>
   </Content>
 </Alert>;
