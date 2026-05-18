36510338f5e2aa37dfba67b93b1525c3c59d735e
Mathis Fullriede <m.fullriede@mittwald.de>
2026-05-18 08:33:40 +0200

    fix(e2e): set empty extBridgeImplementation

--- packages/remote-core/src/ext-bridge/implementation.ts
+++ packages/remote-core/src/ext-bridge/implementation.ts
@@ -1,14 +1,11 @@
-import type { RemoteExtBridgeConnectionApi } from "@/connection/types";
 import { RemoteError } from "@/error";
+import type { ExtBridgeConnectionApi } from "@mittwald/ext-bridge";
 
-export const emptyImplementation: RemoteExtBridgeConnectionApi = {
-  getConfig: async () => ({
-    sessionId: "",
-    userId: "",
-    extensionId: "",
-    extensionInstanceId: "",
-  }),
-  getSessionToken: async () => {
-    throw new RemoteError("Missing implementation for mittwald.extBridge");
+export const emptyImplementation = new Proxy(
+  {},
+  {
+    get() {
+      throw new RemoteError("Missing implementation for mittwald.extBridge");
+    },
   },
-};
+) as ExtBridgeConnectionApi;
