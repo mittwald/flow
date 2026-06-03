f97a2a81a0e29f85c71c85684acc6ce326126d54
Marco Rieger <rieger@racecore.de>
2026-06-03 16:14:44 +0200

    feat(LoadingSpinner): use global clock for starting angle (#2529)
    * use global spinner starting angle

--- packages/components/src/components/LoadingSpinner/LoadingSpinner.tsx
+++ packages/components/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,10 +1,12 @@
-import type { FC } from "react";
+import { type FC } from "react";
 import React from "react";
 import { IconPending } from "@/components/Icon/components/icons";
 import styles from "./LoadingSpinner.module.scss";
 import type { IconProps } from "@/components/Icon";
 import clsx from "clsx";
 import { type AlphaColor, isAlphaColor } from "@/lib/types/props";
+import { useReducedMotion } from "framer-motion";
+import { useDesignTokens } from "@/lib/theming";
 
 export interface LoadingSpinnerProps extends IconProps {
   /** The color of the loading spinner. @default "default" */
@@ -15,13 +17,35 @@ export interface LoadingSpinnerProps extends IconProps {
 export const LoadingSpinner: FC<LoadingSpinnerProps> = (props) => {
   const { className, color = "default", ...rest } = props;
 
+  const preferReducedMotion = useReducedMotion();
+
+  const designTokens = useDesignTokens();
+  const loadingSpinnerTokens = designTokens["loading-spinner"];
+
+  const animationDurationMs = preferReducedMotion
+    ? parseInt(loadingSpinnerTokens["transition-duration-slow"].value)
+    : parseInt(loadingSpinnerTokens["transition-duration"].value);
+
   const rootClassName = clsx(
     styles.loadingSpinner,
     isAlphaColor(color) && styles[color],
     className,
   );
 
-  return <IconPending className={rootClassName} {...rest} />;
+  const startingRotation =
+    ((((performance.now() / animationDurationMs) * 360) % 360) + 360) % 360;
+
+  return (
+    <IconPending
+      className={rootClassName}
+      style={
+        {
+          "--from-angle": `${startingRotation}deg`,
+        } as React.CSSProperties
+      }
+      {...rest}
+    />
+  );
 };
 
 export default LoadingSpinner;
