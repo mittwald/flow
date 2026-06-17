e27de1fc140fcb8abf21e5db91952da39ea2605e
Marco Rieger <rieger@racecore.de>
2026-06-17 08:47:26 +0200

    always render empty views and table actions (#2554)
    fix(List): Modals within an EmptyView or ActionGroup will lose its state when in between loading items

--- packages/components/src/components/List/components/Header/Header.tsx
+++ packages/components/src/components/List/components/Header/Header.tsx
@@ -30,8 +30,19 @@ export const Header: FC<PropsWithClassName> = (props) => {
     list.search ||
     availableViewModes.length > 1;
 
+  const listActions = <UiComponentTunnelExit id="actions" component="List" />;
+
   if (noItemsAvailable) {
-    return;
+    // IMPORTANT: we always render the listActions and returning the same dom tree like 'items are available'!
+    // The correct dom nesting is required since noItemsAvailable makes an early return.
+    // React will not trigger a complete unmount/remount and forces e.g. modalTrigger etc.
+    // inside the Tunnel to get lost when switching from noItems to itemsAvailable and vise versa.
+    // Reminder: A fixed key prop for 'UiComponentTunnelExit' has not the same effect!
+    return (
+      <DivView className={styles.hideVisuallyActions} aria-hidden={true}>
+        <DivView>{listActions}</DivView>
+      </DivView>
+    );
   }
 
   return (
@@ -43,7 +54,7 @@ export const Header: FC<PropsWithClassName> = (props) => {
       )}
     >
       <DivView className={styles.headerContent}>
-        <UiComponentTunnelExit id="actions" component="List" />
+        {listActions}
         {hasOptions && (
           <DivView className={styles.options}>
             <ViewModeContextMenu isDisabled={isDisabled} />
