7ad666139ad799eee3edd0af1c92993d1164f3f4
mathis <37186532+maaaathis@users.noreply.github.com>
2026-06-17 09:31:14 -0400

    fix(docs): getting started typo (#2557)

--- apps/docs/src/content/04-components/navigation/header-navigation/examples/default.tsx
+++ apps/docs/src/content/04-components/navigation/header-navigation/examples/default.tsx
@@ -6,7 +6,7 @@ import {
 } from "@mittwald/flow-react-components";
 
 <HeaderNavigation aria-label="Header navigation">
-  <Link href="#">Getting startet</Link>
+  <Link href="#">Getting started</Link>
   <Link href="#" aria-current="page">
     Komponenten
   </Link>
