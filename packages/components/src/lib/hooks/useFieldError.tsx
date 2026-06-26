bb1ecb7f2ba608f19d2ad8ef190754020168c3d5
Marco Rieger <m.rieger@mittwald.de>
2026-06-26 17:13:21 +0200

    pass aria props for invalid fields

--- packages/components/src/lib/hooks/useFieldError.tsx
+++ packages/components/src/lib/hooks/useFieldError.tsx
@@ -1,4 +1,4 @@
-import React, { type FC, type PropsWithChildren, useId, useMemo } from "react";
+import React, { type FC, type PropsWithChildren, useMemo } from "react";
 import { type PropsContext, PropsContextProvider } from "@/lib/propsContext";
 import formFieldStyles from "@/components/FormField/FormField.module.scss";
 import ClearPropsContext from "@/lib/propsContext/components/ClearPropsContext";
@@ -7,17 +7,20 @@ import { UiComponentTunnelExit } from "@/components/UiComponentTunnel/UiComponen
 import type { FlowComponentName } from "@/components/propTypes";
 
 export interface UseFieldErrorOptions {
+  fieldErrorId: string;
   tunnelId?: string;
   component: FlowComponentName;
 }
 
 export const useFieldError = (options: UseFieldErrorOptions) => {
-  const id = useId();
-  const currentTunnelId = useProps("FieldError", {}).tunnel?.id;
-  const tunnelId = options.tunnelId ?? currentTunnelId ?? `${id}.fieldError`;
+  const fieldErrorProps = useProps("FieldError", {});
+  const currentTunnelId = fieldErrorProps.tunnel?.id;
+  const tunnelId =
+    options.tunnelId ?? currentTunnelId ?? `${options.fieldErrorId}.fieldError`;
 
   const fieldErrorCapturePropsContext: PropsContext = {
     FieldError: {
+      id: options.fieldErrorId,
       tunnel: { id: tunnelId, component: options.component },
       className: formFieldStyles.fieldError,
     },
