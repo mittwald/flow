b6809b24d76b4435d65ce183a0d6c5df0b6aaa2a
Marco Rieger <rieger@racecore.de>
2026-06-29 08:39:49 +0200

    feat(FieldError): announce fieldError message to aria (#2581)

--- packages/components/src/lib/hooks/useFieldComponent.tsx
+++ packages/components/src/lib/hooks/useFieldComponent.tsx
@@ -1,4 +1,4 @@
-import type { FC, PropsWithChildren } from "react";
+import { type FC, type PropsWithChildren, useId } from "react";
 import { type PropsContext } from "@/lib/propsContext";
 import formFieldStyles from "@/components/FormField/FormField.module.scss";
 import { useFieldError } from "@/lib/hooks/useFieldError";
@@ -9,6 +9,8 @@ interface FieldComponentProps {
   className?: ClassValue;
   isRequired?: boolean;
   isDisabled?: boolean;
+  isInvalid?: boolean;
+  "aria-describedby"?: string;
 }
 
 export interface UseFieldComponent {
@@ -16,6 +18,7 @@ export interface UseFieldComponent {
   FieldErrorView: FC;
   fieldPropsContext: PropsContext;
   fieldProps: {
+    "aria-describedby"?: string;
     className?: ReturnType<typeof clsx>;
   };
 }
@@ -24,7 +27,9 @@ export const useFieldComponent = (
   props: FieldComponentProps,
   component: FlowComponentName,
 ): UseFieldComponent => {
+  const fieldErrorId = useId();
   const { FieldErrorView, FieldErrorCaptureContext } = useFieldError({
+    fieldErrorId,
     component,
   });
 
@@ -46,6 +51,9 @@ export const useFieldComponent = (
     FieldErrorCaptureContext,
     fieldPropsContext,
     fieldProps: {
+      "aria-describedby": props.isInvalid
+        ? fieldErrorId
+        : props["aria-describedby"],
       className: clsx(formFieldStyles.formField),
     },
   } as const;
