de72c803799c24f4de130518ff66a182c2156d3e
Marco Rieger <rieger@racecore.de>
2026-08-11 06:57:40 +0200

    chore(dev): prepare for typescript 7 (#2827)
    * add typescript 7 migration package to run v6 and v7 in parallel + pnpm 11 + run lint on push not commit
    
    * fix ssr mismatch
    
    * swap to @teispace/next-themes in favor of unmaintained next-themes for ssr
    
    * initialize theme
    
    * cleanup

--- apps/remote-dom-demo/next.config.js
+++ apps/remote-dom-demo/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   basePath: process.env.NEXT_BASE_PATH ?? "",
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   experimental: {
+    useTypeScriptCli: true,
     serverActions: {
       bodySizeLimit: "100mb",
     },
